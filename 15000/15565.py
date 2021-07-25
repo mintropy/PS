c4661deeada408d5ac8dbb5a19578f14c4d53c7e
mintropy <jun2korea@gmail.com>
2021-07-25 14:21:37 +0900

    15565 solved

--- 15000/15565.py
+++ 15000/15565.py
@@ -10,6 +10,36 @@ input = sys.stdin.readline
 n, k = map(int, input().split())
 seq = list(map(int, input().split()))
 
+left, right = 0, 0
+if seq[0] == 1:
+    lion_count = 1
+else:
+    lion_count = 0
+min_subseq_len = int(1e7)
+
+while left <= right:
+    # 라이언 인형 개수 만족 & 더 짧은 개수 >> 최신화
+    if lion_count >= k:
+        if min_subseq_len > right - left + 1:
+            min_subseq_len = right - left + 1
+        # 개수가 같거나 많은경우, left 이동
+        if seq[left] == 1:
+            lion_count -= 1
+        left += 1
+    elif lion_count < k:
+        if right == n - 1:
+            break
+        right += 1
+        if seq[right] == 1:
+            lion_count += 1
+
+if min_subseq_len > n:
+    print(-1)
+else:
+    print(min_subseq_len)
+
+'''
+# 포인터 설정이 조금 잘못된 듯
 # 최솟값
 min_sub_seq = int(1e6)
 
@@ -35,3 +65,4 @@ while True:
             lion_count += 1
 
 print(min_sub_seq)
+'''
