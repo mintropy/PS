49a9c9a84932106460f9a33ad8875106a50575de
mintropy <jun2korea@gmail.com>
2021-07-27 14:40:46 +0900

    2805 solved

--- 2000/2805.py
+++ 2000/2805.py
@@ -10,19 +10,29 @@ import sys
 input = sys.stdin.readline
 
 def bin_search(m, trees):
+    # 자른 높이, 가져가는 길이
+    close = [0, sum(trees)]
     minimum = 1
     maximum = max(trees)
     mid = maximum // 2
-    while minimum < maximum:
+    while minimum <= maximum:
         mid = (minimum + maximum) // 2
         count = sum(tree - mid if tree > mid else 0 for tree in trees)
+        # 정확한 개수를 모았을 때
         if count == m:
             break
-        elif count > m:
+        # 탐색 실패 대비
+        if count > m and count < close[1]:
+            close = [mid, count]
+        # 다음 이분 탐색
+        if count > m:
             minimum = mid + 1
         elif count < m:
             maximum = mid - 1
-    return max(maximum, minimum)
+    if count < m or m < close[1] < count:
+        return close[0]
+    else:
+        return mid
 
 
 n, m = map(int, input().split())
