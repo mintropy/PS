b1db49dc8b87ff806ce11ddc0289e814f73b1add
mintropy <jun2korea@gmail.com>
2021-07-31 22:42:42 +0900

    1799 solving

--- 1000/1799.py
+++ 1000/1799.py
@@ -7,18 +7,66 @@ import sys
 input = sys.stdin.readline
 
 
-def check(chess: list, i: int, j: int) -> bool:
-    direction = {1: (-1, 1), 2: (1, 1), 3: (1, -1), 4: (-1, -1)}
-    # 어짜피 비숍을 위에서부터 확인, 놓고 있으니 위에로만 확인해도 괜찮지 않을까?
-    # for d in range(1, 4 + 1):
-    for d in [1, 4]:
-        for k in range(1, n):
-            x, y = i + direction[d][0] * k, j + direction[d][1] * k
-            if x < 0 or x >= n:
-                break
-            if y < 0 or y >= n:
-                break
-            if chess[x][y] == 2:
+def dfs(bishop_count: int, bishop_idx: int):
+    global n, max_bishop, bishop_able, ables, check_diag_1, check_diag_2
+    if bishop_idx == ables:
+        if bishop_count > max_bishop:
+            max_bishop = bishop_count
+        return
+    # 비숍을 추가하지 않고 탐색
+    dfs(bishop_count, bishop_idx + 1)
+    # 해당 비숍을 놓을 수 있는지 확인 > 추가하고 탐색
+    # 몇번째 대각선인지 찾기
+    # diag_1은 두 좌표의 합으로 해당 대각선 지정
+    # diag_2는 n - 1 - j + i로 지정
+    i, j = bishop_able[bishop_idx]
+    diag_1_idx = i + j
+    diag_2_idx = n - 1 - j + i
+    # 두 대각선 모두 놓을 수 있으면 진행
+    if not check_diag_1[diag_1_idx] and not check_diag_2[diag_2_idx]:
+        check_diag_1[diag_1_idx] = True
+        check_diag_2[diag_2_idx] = True
+        dfs(bishop_count + 1, bishop_idx + 1)
+        check_diag_1[diag_1_idx] = False
+        check_diag_2[diag_2_idx] = False
+
+
+n = int(input())
+chess = list(list(map(int, input().split())) for _ in range(n))
+
+# 각 대각선이 가능한지만 확인하면 됨
+# 각 대각선에 놓게되면 True로 변경
+# 1은 오른쪽 위에서 왼쪽 아래로
+# 2는 왼쪽 위에서 오른쪽 아래로 가는 대각선
+# 대각선의 개수 : (n - 1) * 2 + 1
+check_diag_1 = [False] * ((n - 1) * 2 + 1)
+check_diag_2 = [False] * ((n - 1) * 2 + 1)
+
+bishop_able = []
+for i in range(n):
+    for j in range(n):
+        if chess[i][j] == 1:
+            bishop_able.append((i, j))
+#  가능한 자리의 수 == 비숍을 놓을 수 있는 자리의 개수
+ables = len(bishop_able)
+
+max_bishop = 0
+
+dfs(0, 0)
+print(max_bishop)
+
+
+'''
+def check(bishop_idx: list) -> bool:
+    global bishop_able, bishop_used
+    # 어짜피 대각선에 있으니깐, 한칸씩 이동하며 탐색하는 대신
+    # 좌표사이 차이로만 비교해도 괜찮다 !!
+    # & 비숍을 위에서부터 확인했으니, 해당 인댁스 이전까지만 확인
+    i, j = bishop_able[bishop_idx]
+    for k in range(bishop_idx):
+        if bishop_used[k]:
+            x, y = bishop_able[k]
+            if abs(i - x) == abs(j - y):
                 return False
     return True
 
@@ -31,13 +79,13 @@ def dfs(chess: list, bishop_idx: list, bishop_count: int):
         return
     # 해당 인덱스의 비숍을 사용하는 것, 사용하지 않는 것으로 dfs 실시
     dfs(chess, bishop_idx + 1, bishop_count)
-    i, j = bishop_able[bishop_idx]
-    if check(chess, i, j):
-        chess[i][j] = 2
+    if check(bishop_idx):
+        bishop_used[bishop_idx] = True
         dfs(chess, bishop_idx + 1, bishop_count + 1)
-        chess[i][j] = 1
+        bishop_used[bishop_idx] = False
 
 
+# 체스판에 모두 기록할 필요가 없다
 n = int(input())
 chess = list(list(map(int, input().split())) for _ in range(n))
 
@@ -46,15 +94,16 @@ for i in range(n):
     for j in range(n):
         if chess[i][j] == 1:
             bishop_able.append((i, j))
+#  가능한 자리의 수 == 비숍을 놓을 수 있는 자리의 개수
 ables = len(bishop_able)
+bishop_used = [False] * ables
 
 max_bishop = 0
 
 
 dfs(chess, 0, 0)
 print(max_bishop)
-
-
+'''
 
 
 '''
