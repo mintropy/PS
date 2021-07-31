808ed636c2e2c37584ebada67973be1493f72882
mintropy <jun2korea@gmail.com>
2021-07-31 14:20:12 +0900

    2346 solving

---
+++ 2000/2346.py
@@ -0,0 +1,20 @@
+"""
+Title : 풍선 터트리기
+Link : https://www.acmicpc.net/problem/2346
+"""
+
+import sys, collections
+
+n = int(input())
+queue = collections.deque([i for i in range(1, n + 1)])
+
+cmd = list(map(int, input().split()))
+
+for i in range(n):
+    m = queue.popleft()
+    print(m, end = ' ')
+    c = cmd[m - 1]
+    if c > 0:
+        queue.rotate(-1 * (c - 1))
+    else:
+        queue.rotate(-1 * (c + 2))
