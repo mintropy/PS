d0da74039f2dd0b974afbc8443c031f50cc8b15f
mintropy <jun2korea@gmail.com>
2021-08-08 14:17:21 +0900

    2098 solving

--- 2000/2000/2098.py
+++ 2000/2000/2098.py
@@ -1,7 +1,56 @@
-import sys
+"""
+Title : 외판원 순회
+Link : https://www.acmicpc.net/problem/2098
+"""
 
+import sys, itertools
 input = sys.stdin.readline
 
+
+def tsp(city_visited: int, city_count: int, city_now: int):
+    if city_count == 2:
+        return adjacency_matrix[0][city_now]
+    min_costs = []
+    # 이미 탐색한 도시부터 되돌아가며 탐색
+    for i in range(1, n):
+        # 해당 도시이거나, 방문하지 않은도시이면 건너뛰기
+        if i == city_now:
+            continue
+        if not (1 << i & city_visited):
+            continue
+        # i번 도시에서 city_now까지 비용
+        c = adjacency_matrix[i][city_now]
+        if c == 0:
+            continue
+        # i번 도시를 방문하지 영우
+        city_not_i = (1 << n + 1) - 1
+        city_not_i = city_not_i & ~(1 << i)
+        cost_not_i = tsp(city_not_i, city_count - 1, i)
+        min_costs.append(cost_not_i + c)
+    if min_costs:
+        return min(min_costs)
+    else:
+        return 0
+
+
+n = int(input())
+adjacency_matrix = [list(map(int, input().split())) for _ in range(n)]
+# 마지막 도시 >> 처음도시(0)으로 돌아가는건 따로 계산
+min_cost = 10 ** 8
+for c in range(2, n):
+    cost = tsp((1 << n + 1) - 1, n, c)
+    if cost == 0:
+        continue
+    cost += adjacency_matrix[c][0]
+    if cost < min_cost:
+        min_cost = cost
+
+print(min_cost)
+
+
+
+'''
+# 전체 탐색 : 시간 초과
 def TSP(dp: list, visit: int, now: int):
     global n, w, ans
     # 모든 점을 방문 했을 때
@@ -25,3 +74,4 @@ dp[0][1] = 0
 ans = INF
 TSP(dp, 1, 0)
 print(ans)
+'''
