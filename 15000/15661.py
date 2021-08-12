84008bf74a301dccddb12e918216579c97cf4996
mintropy <jun2korea@gmail.com>
2021-08-12 16:11:50 +0900

    15661 solving

---
+++ 15000/15661.py
@@ -0,0 +1,12 @@
+"""
+Title : 링크와 스타트
+Link : https://www.acmicpc.net/problem/15661
+"""
+
+import sys, itertools
+input = sys.stdin.readline
+
+n = int(input())
+ability = [list(map(int, input().split())) for _ in range(n)]
+
+
