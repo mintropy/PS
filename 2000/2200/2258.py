97cc01b1fa0372971dc3a507708125b8fb4682b5
mintropy <jun2korea@gmail.com>
2021-08-14 23:22:28 +0900

    2258 solving

--- 2000/2200/2258.py
+++ 2000/2200/2258.py
@@ -7,38 +7,52 @@ import sys, heapq
 input = sys.stdin.readline
 
 
-def check(p_before, p_after, total_weight, weights, new_weight):
-    global m
-    if total_weight + sum(weights) < m:
-        return False
-
-    pass
-
-
-
 n, m = map(int, input().split())
 meat = []
+total_weight = 0
 for _ in range(n):
     w, p = map(int, input().split())
-    meat.append((p, w))
+    total_weight += w
+    meat.append((-p, p, w))
 
 heapq.heapify(meat)
 
-total_weight = 0
-weights = []
-price_now = 0
-prob = [0, 0]
+min_cost = 3 * 10 ** 8
 
-for _ in range(n):
-    p, w = heapq.heappop(meat)
-    # 가격이 달라지면, 다시 계산 시작
-    if p == price_now:
-        weights.append(total_weight)
-    else:
-        pass
-    total_weight += w
-    if total_weight >= m:
-        print(p)
+while meat:
+    _, p, w = heapq.heappop(meat)
+    # 종료조건
+    if p > min_cost:
         break
-else:
+    if total_weight < m:
+        break
+    weights = [w]
+    weights_same_price = w
+    while True:
+        if not meat and meat[0][1] == p:
+            weights.append(w)
+            weights_same_price += w
+        else:
+            break
+    
+    # 전체 무게에서 지금 가격 전체 무게 뺐을때
+    # m을 넘어가는 경우과, 넘지않는 경우로 구분
+    if total_weight - weights_same_price >= m:
+        # 해당 가격으로 최소가격 저장
+        min_cost = p
+    else:
+        # 해당 가격 일부만 구매해서 원하는 무게를 넘는 수 찾기
+        weight_diff = total_weight - m
+        # 해당 무게로 필요한 개수
+        meat_count = weight_diff // w + 1
+        p2 = p * meat_count
+        # 가격 확인
+        if p2 < min_cost:
+            min_cost = p2
+    # 무게 최신화
+    total_weight -= weights_same_price
+
+if min_cost == 3 * 10 ** 8:
     print(-1)
+else:
+    print(min_cost)
