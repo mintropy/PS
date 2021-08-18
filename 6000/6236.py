f8efc5dcc55eb27dcf9f5ee6e75376b2df78d9d4
mintropy <jun2korea@gmail.com>
2021-08-18 17:13:27 +0900

    6236 solved

--- 6000/6236.py
+++ 6000/6236.py
@@ -8,37 +8,71 @@ input = sys.stdin.readline
 
 
 def bin_search(m: int, money: list) -> int:
-    st, end = 0, sum(money)
+    st, end = max(money), sum(money)
     ans = sum(money)
     while st <= end:
         mid = (st + end) // 2
-        # 출금 횟수
-        withdraw = count_withdraw(money, mid)
-        if withdraw >= m:
-            if mid < ans and withdraw != 10 ** 6:
+        # m번 출금 가능할 때, 더 작은 금액 가능한지 확인
+        if count_withdraw(m, money, mid):
+            if mid < ans:
                 ans = mid
-            st = mid + 1
-        else:
             end = mid - 1
+        else:
+            st = mid + 1
     return ans
 
 
-def count_withdraw(money: list, mid: int) -> int:
-    withdraw = 1
+# m번 인출이 가능한지 확인
+def count_withdraw(withdraw_count: int, money: list, mid: int) -> int:
+    # 0번에서 확인해가는 대신, 모든 날 인출하는 것에서 줄일 수 있는지 확인
+    withdraw = len(money)
     money_now = mid
+    money_for_day_count = 0
     for m in money:
         if m > money_now:
-            withdraw += 1
-            if m > mid:
-                return 10 ** 6
+            withdraw -= money_for_day_count - 1
+            # 출금 제한 횟수보다 더 적게 가능하면 True
+            # 압축한 날을 뽑아 둘로 나누는 작업하면 개수 맞출 수 있음
+            if withdraw <= withdraw_count:
+                return True
+            money_for_day_count = 1
             money_now = mid - m
         else:
+            money_for_day_count += 1
             money_now -= m
-    return withdraw
-
+    # 마지막 날까지 탐색하고 한번 더 확인
+    withdraw -= money_for_day_count - 1
+    if withdraw <= withdraw_count:
+        return True
+    else:
+        return False
 
 
 n, m = map(int, input().split())
 money = list(int(input()) for _ in range(n))
 
 print(bin_search(m, money))
+
+
+'''
+Counter Example
+5 3
+100
+100
+100
+100
+100
+ans : 200
+output : 100
+
+
+5 2
+100
+100
+100
+100
+100
+ans : 300
+output : 400
+
+'''
