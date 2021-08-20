4fec7c2478bebd8b0ad4b3dc66ad8d8d0acf5d0b
mintropy <jun2korea@gmail.com>
2021-08-20 09:59:41 +0900

    2457 solved

--- 2000/2400/2457.py
+++ 2000/2400/2457.py
@@ -19,7 +19,6 @@ def check(m1, d1, m2, d2):
     return False
 
 
-
 n = int(input())
 flowers = [tuple(map(int, input().split())) for _ in range(n)]
 
@@ -36,13 +35,27 @@ else:
     blossom = [m1, d1]
     # 그 전에 선택할 꽃 예비 명단
     prob = []
+    idx = 0
     for i in range(1, n):
         # 12월에 지는 꽃일때 확인 필요
-        
+        # 12월 꽃인경우 모두 확인
+        if flowers[i][2] == 12:
+            # 꽃이 더 빨리 핀다면 바꾸기
+            if check(*flowers[i][:2], *blossom):
+                blossom = flowers[i][:2]
+        else:
+            idx = i
+            break
+    # 12월에 피는 꽃이 아닐때부터 탐색 시작
+    for i in range(idx, n):
         m1, d1, m2, d2 = flowers[i]
-        # 만약 예비꽃이 3월 1일 이전에 핀다면 종료
-        if prob and check(*prob, 3, 1):
-            print(flower_count + 1)
+        # 피는날 지는날 같은 경우 없음
+        # 피는날 지는날 같으면 넘어가기
+        # if m1 == m2 and d1 == d2:
+        #     continue
+        # 만약 지금꽃이 3월 1일 이전에 핀다면 종료
+        if prob and check(*blossom, 3, 1):
+            print(flower_count)
             break
         # 해당 꽃이 가능하면 예비명단과 비교 최신화
         if check(*blossom, m2, d2):
@@ -64,8 +77,38 @@ else:
                 print(0)
                 break
     else:
-        # 모든 꽃을 확인한 경우, 예비꽃을 확인
-        if check(*prob, 3, 1):
+        # 모든 꽃을 확인한 경우, 지금꽃, 예비꽃을 확인
+        # 마지막에 지금꽃이 된경우 꽃 확인 개수 출력
+        # 마지막까지 지금꽃으로 안되고, 예비꽃이 되는 경우
+        # 꽃 확인개수 + 1 출력
+        if check(*blossom, 3, 1):
+            print(flower_count)
+        elif check(*prob, 3, 1):
             print(flower_count + 1)
         else:
             print(0)
+
+
+
+'''
+Counter Example
+10
+1 1 11 23
+11 22 11 24
+11 23 11 25
+11 24 11 26
+11 25 11 27
+11 26 11 28
+11 27 11 29
+11 28 12 1
+11 23 11 27
+11 27 12 1
+ans : 3
+output : 2
+
+
+3
+11 1 12 1
+3 1 11 2
+1 1 3 5
+'''
