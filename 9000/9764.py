992a12eec26a178be5b9262336f72459e869f34d
mintropy <jun2korea@gmail.com>
2021-08-28 15:31:00 +0900

    9764 solving

--- 9000/9764.py
+++ 9000/9764.py
@@ -7,11 +7,18 @@ import sys
 input = sys.stdin.readline
 
 dp = [[0] * 2001 for _ in range(2001)]
+dp[1][1] = 1
+dp[2][1] = dp[2][2] = 1
 
-for i in range(1, 2001):
+for i in range(3, 2001):
     dp[i][i] = 1
-    
-for i in range(1, 2001):
-    for j in range(1, i):
-        # 범위 확인
-        pass
+    for j in range(i - 1, 0, - 1):
+        if (j * (j + 1)) // 2 < i:
+            dp[i][j] = dp[i][j + 1]
+        elif j > i - j:
+            dp[i][j] = (dp[i][j + 1] + dp[i - j][1]) % 100999
+        else:
+            dp[i][j] = (dp[i][j + 1] + (dp[j][1] - dp[j][j])) % 100999
+
+for _ in range(int(input())):
+    print(dp[int(input())][1])
