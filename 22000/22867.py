d9526dc9e7d1e7a7db4ddc20e94034e2bae2f5eb
mintropy <jun2korea@gmail.com>
2021-08-31 23:15:35 +0900

    22867 solving

--- 22000/22867.py
+++ 22000/22867.py
@@ -10,29 +10,40 @@ input = sys.stdin.readline
 def check(leave_last: str, next_start: str) -> bool:
     # 시, 분, 초, 밀리초 순서로 탐색
     # idx : :2, 3:5, 6:8, 9:
-    if next_start[:2] < leave_last[:2]:
+    if int(next_start[:2]) < int(leave_last[:2]):
         return False
-    elif next_start[3:5] < leave_last[3:5]:
+    elif int(next_start[3:5]) < int(leave_last[3:5]):
         return False
-    elif next_start[6:8] < leave_last[6:8]:
+    elif int(next_start[6:8]) < int(leave_last[6:8]):
         return False
-    elif next_start[9:] < leave_last[9:]:
+    elif int(next_start[9:]) < int(leave_last[9:]):
         return False
     return True
 
 
 n = int(input())
 # HH:MM:SS.sss
-schedule = sorted(list(tuple(input().strip().split()) for _ in range(n)), key=lambda x: x[1])
+schedule = []
+for _ in range(n):
+    arrival, leave = input().strip().split()
+    arrival = arrival.replace(':', '')
+    arrival = arrival.replace('.', '')
+    leave = leave.replace(':', '')
+    leave = leave.replace('.', '')
+    arrival, leave = int(arrival), int(leave)
+    schedule.append((arrival, leave))
+    if leave < arrival:
+        schedule.append((0, leave))
 
-count = 1
-train_leave = schedule[0][1]
+schedule.sort()
+max_area = 1
+for i in range(n):
+    if i + max_area >= n:
+        break
+    for j in range(i + 1, n):
+        if schedule[j][0] > schedule[i][1]:
+            break
+    if max_area < j - i:
+        max_area = j - i
 
-for i in range(1, n):
-    st, end = schedule[i]
-    if check(train_leave, st):
-        train_leave = end
-    else:
-        count += 1
-
-print(count)
+print(max_area)
