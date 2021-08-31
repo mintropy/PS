4dfba78a9c4b951f325a8879851d68bfa4ad66b0
mintropy <jun2korea@gmail.com>
2021-08-31 18:26:47 +0900

    14287 solved

---
+++ 14000/14287.py
@@ -0,0 +1,28 @@
+"""
+Title : 회사 문화 3
+Link : https://www.acmicpc.net/problem/14287
+"""
+
+import sys
+input = sys.stdin.readline
+
+
+def update(i, w):
+    while i != -1:
+        company[i] += w
+        i = sup[i]
+    return
+
+
+n, m = map(int ,input().split())
+company = [0] * (n + 1)
+sup = [0] + list(map(int, input().split()))
+
+
+for _ in range(m):
+    query, *todo = map(int, input().split())
+    
+    if query == 1:
+        update(*todo)
+    else:
+        print(company[todo[0]])
