6ede6c17b9da22b1512fe3b023ba28e060707eb2
mintropy <jun2korea@gmail.com>
2021-09-02 16:48:24 +0900

    22867 solved

--- 22000/22867.py
+++ 22000/22867.py
@@ -3,47 +3,40 @@ Title : 종점
 Link : https://www.acmicpc.net/problem/22867
 """
 
-import sys
+import sys, heapq
 input = sys.stdin.readline
-
-
-def check(leave_last: str, next_start: str) -> bool:
-    # 시, 분, 초, 밀리초 순서로 탐색
-    # idx : :2, 3:5, 6:8, 9:
-    if int(next_start[:2]) < int(leave_last[:2]):
-        return False
-    elif int(next_start[3:5]) < int(leave_last[3:5]):
-        return False
-    elif int(next_start[6:8]) < int(leave_last[6:8]):
-        return False
-    elif int(next_start[9:]) < int(leave_last[9:]):
-        return False
-    return True
 
 
 n = int(input())
 # HH:MM:SS.sss
-schedule = []
-for _ in range(n):
-    arrival, leave = input().strip().split()
-    arrival = arrival.replace(':', '')
-    arrival = arrival.replace('.', '')
-    leave = leave.replace(':', '')
-    leave = leave.replace('.', '')
-    arrival, leave = int(arrival), int(leave)
-    schedule.append((arrival, leave))
-    if leave < arrival:
-        schedule.append((0, leave))
-
-schedule.sort()
-max_area = 1
+schedual = []
 for i in range(n):
-    if i + max_area >= n:
-        break
-    for j in range(i + 1, n):
-        if schedule[j][0] > schedule[i][1]:
-            break
-    if max_area < j - i:
-        max_area = j - i
+    a, l = input().strip().split()
+    a = a.replace(':', '')
+    a = a.replace('.', '')
+    l = l.replace(':', '')
+    l = l.replace('.', '')
+    schedual.append((a, l))
 
-print(max_area)
+schedual.sort()
+max_terminal = 1
+terminal_now = 0
+terminal = []
+for a, l in schedual:
+    if not terminal:
+        heapq.heappush(terminal, l)
+        terminal_now += 1
+    else:
+        if a >= terminal[0]:
+            while terminal:
+                if a >= terminal[0]:
+                    heapq.heappop(terminal)
+                    terminal_now -= 1
+                else:
+                    break
+        heapq.heappush(terminal, l)
+        terminal_now += 1
+        if terminal_now > max_terminal:
+            max_terminal = terminal_now
+
+print(max_terminal)
