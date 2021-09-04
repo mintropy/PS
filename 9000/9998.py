1a1321fd99d8098e39d383f60e829772022b9fb4
mintropy <jun2korea@gmail.com>
2021-09-04 11:56:51 +0900

    9998 solving

---
+++ 9000/9998.py
@@ -0,0 +1,13 @@
+"""
+Title : 블록 쌓기
+Link : https://www.acmicpc.net/problem/9998
+"""
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+yoon = list(map(int, input().split()))
+dong = list(map(int, input().split()))
+
+
