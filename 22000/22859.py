9bd463a56c0b43d4d9d99adee456f3742a7b8741
mintropy <jun2korea@gmail.com>
2021-09-04 14:46:53 +0900

    22859 solving

--- 22000/22859.py
+++ 22000/22859.py
@@ -6,30 +6,27 @@ Link : https://www.acmicpc.net/problem/22859
 html = list(input().strip().split('<'))
 
 ans = []
-for i in range(2, len(html)):
+for i in range(2, len(html) - 1):
     # div, main닫는 태그면 넘어가기
     if html[i][0] == '/':
         if html[i][1] == 'i' or html[i][1] == 'b':
-            tmp = html[i][3:].strip()
-            if tmp:
-                ans[-1] += ' ' + tmp
+            _, tmp = html[i].split('>')
+            if tmp != ' ' * len(tmp):
+                ans[-1] += ' ' + tmp.strip()
         else:
             continue
     # div 태그면 title로 바로 추가
     elif html[i][0] == 'd':
-        ans.append('title : ' + html[i][11:-2])
+        tmp = html[i].split('"')
+        ans.append('title : ' + tmp[1])
     # p태그이면 새롭게 추가, 아니면 같은 줄이므로, 공백 후 이전 내용에 추가
     elif html[i][0] == 'p':
-        ans.append(html[i][2:].strip())
-    elif html[i][0:2] == 'br':
-        tmp = html[i][4:].strip()
-        if tmp:
-            ans[-1] += ' ' + tmp
+        _, tmp = html[i].split('>')
+        ans.append(tmp.strip())
+    # i, br 태그일 때
     else:
-        tmp = html[i][2:].strip()
-        if tmp:
-            ans[-1] += ' ' + tmp
+        _, tmp = html[i].split('>')
+        if tmp != ' ' * len(tmp):
+            ans[-1] += ' ' + tmp.strip()
 
-for line in ans:
-    if line:
-        print(line)
+print(*ans, sep='\n')
