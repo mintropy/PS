370f1934b9e55d2e9b3a7ba707e35f65c1e5b5c2
mintropy <jun2korea@gmail.com>
2021-09-05 21:41:05 +0900

    9998 solving

--- 9000/9998.py
+++ 9000/9998.py
@@ -10,4 +10,39 @@ n = int(input())
 yoon = list(map(int, input().split()))
 dong = list(map(int, input().split()))
 
+ans = [0] * n
+ans[n // 2] = (yoon[n // 2] + dong[n // 2]) // 2
+action = abs(ans[n // 2] - yoon[n // 2]) + abs(ans[n // 2] - dong[n // 2])
 
+for i in range(1, n // 2 + 1):
+    # 왼쪽
+    left = (yoon[n // 2 - i] + dong[n // 2 - i]) // 2
+    if left <= ans[n // 2 - i + 1]:
+        ans[n // 2 - i] = ans[n // 2 - i + 1] + 1
+    else:
+        ans[n // 2 - i] = left
+    action += abs(ans[n // 2 - i] - yoon[n // 2 - i]) + abs(ans[n // 2 - i] - dong[n // 2 - i])
+    # 오른쪽
+    right = (yoon[n // 2 + i] + dong[n // 2 + i]) // 2
+    if right <= ans[n // 2 + i - 1]:
+        ans[n // 2 + i] = ans[n // 2 + i - 1] + 1
+    else:
+        ans[n // 2 + i] = right
+    action += abs(ans[n // 2 - i] - yoon[n // 2 + i]) + abs(ans[n // 2 - i] - dong[n // 2 + i])
+
+print(action)
+
+
+'''
+Counter Example
+5
+3 2 5 2 3
+3 2 4 2 3
+ans : 7
+
+5
+3 2 5 8 15
+3 2 5 8 15
+
+
+'''
