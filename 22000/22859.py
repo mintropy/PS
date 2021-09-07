063d8b03f2fa28fac405fe8f5ed7c7093d84fd29
mintropy <jun2korea@gmail.com>
2021-09-07 14:54:14 +0900

    22859 solving

--- 22000/22859.py
+++ 22000/22859.py
@@ -3,30 +3,86 @@ Title : HTML 파싱
 Link : https://www.acmicpc.net/problem/22859
 """
 
+html = input().strip()
+
+
+
+
+'''
+html = input().strip().split('<div')
+
+lines = []
+for i in range(1, len(html)):
+    lines.append(html[i].split('<p>'))
+
+ans = []
+for div in lines:
+    if 'title="'in div[0]:
+        title = div[0].split('"')
+        ans.append('title : ' + title[1])
+        st = 1
+    else:
+        st = 0
+    
+    for i in range(st, len(div)):
+        paragraph = div[i]
+        tmp = ''
+        tag = False
+        for s in paragraph:
+            if s == '<':
+                tag = True
+            elif s == '>':
+                tag = False
+            elif tag:
+                continue
+            else:
+                if s == ' ':
+                    if len(tmp) == 0 or tmp[-1] == ' ':
+                        continue
+                tmp += s
+        if tmp:
+            ans.append(tmp)
+
+print(*ans, sep='\n')
+'''
+
+
+'''
+# index error?
 html = input().strip().split('<div')
 ans = []
 
 for context in html[1:]:
+    if not context:
+        continue
     tmp = context.split('<p>')
     # 제목은 따로 추가
     title = tmp[0].split('"')
     ans.append('title : ' + title[1])
+    if len(context) == 1:
+        continue
     # 각 줄 추가
-    # if len(tmp) > 1:
     for paragraph in tmp[1:]:
         para = ''
-        st = 0
+        tag = False
         for i in range(len(paragraph)):
-            if paragraph[i] == '<':
-                txt = paragraph[st:i].strip()
-                if txt != ' ' * len(txt):
-                    para += ' ' + paragraph[st:i].strip()
-            elif paragraph[i] == '>':
-                st = i + 1
-        ans.append(para.strip())
+            if paragraph[i] == '>':
+                tag = False
+            elif paragraph[i] == '<' or tag:
+                tag = True
+                continue
+            else:
+                c = paragraph[i]
+                if c == ' ':
+                    if not para or para[-1] == ' ':
+                        continue
+                para += c
+        para.strip()
+        if para:
+            ans.append(para)
 
 print(*ans, sep='\n')
-
+'''
 
 '''
 # index error
@@ -51,5 +107,6 @@ print(*ans, sep='\n')
 
 '''
 <main><div title="title"></div></main>
+<main><div title="title"></div><p>para</p></main>
 
 '''
