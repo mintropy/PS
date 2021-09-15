eca4a86ac4385f2ec0dedbdb131ee6e2e3649b5e
mintropy <jun2korea@gmail.com>
2021-09-15 18:52:38 +0900

    21737 solved

--- 21000/21737.py
+++ 21000/21737.py
@@ -8,18 +8,21 @@ input = sys.stdin.readline
 
 
 def operate(num1: int, num2: int, operation: str) -> int:
-    if operation_last == 'S':
+    """
+    num1 operation num2 의 결과값 반환
+    """
+    if operation == 'S':
         return num1 - num2
-    elif operation_last == 'M':
+    elif operation == 'M':
         return num1 * num2
-    elif operation_last == 'U':
+    elif operation == 'U':
         if num1 >= 0:
             return num1 // num2
         else:
             num1 *= -1
             num1 //= num2
             return num1 * -1
-    elif operation_last == 'P':
+    elif operation == 'P':
         return num1 + num2
 
 
@@ -27,11 +30,13 @@ n = int(input())
 poly = input().strip()
 
 idx = 0
+# 연산자 이전에 있는 숫자
+# 연산자가 나오면 num_now를 num_last로
 num_last = 0
 operation_last = ''
+# 다음 연산자 나오기 까지
+# C나오면 num_now 출력
 num_now = 0
-# num_now에 입력하고 있는지, 새로 입력해야 하는지
-new_num = False
 output = []
 
 while idx < len(poly):
@@ -40,30 +45,24 @@ while idx < len(poly):
         if poly[idx] == 'C':
             if operation_last:
                 num_now = operate(num_last, num_now, operation_last)
-                new_num = False
                 output.append(num_now)
                 operation_last = ''
             else:
                 output.append(num_now)
-        # 처음 연산자를 만났을 때
-        elif operation_last == '':
-            num_last = num_now
-            operation_last = poly[idx]
+        # 연산자를 만났고, 이전 연산자가 있을 때
+        elif operation_last:
+            num_last = operate(num_last, num_now, operation_last)
             num_now = 0
-        # 아니라면 이전 연산을 하고, 연산자 새로 추가
+            operation_last = poly[idx]
+        # 연산자를 만났고, 이전 연산자가 없을 때
         else:
-            num_now = operate(num_last, num_now, operation_last)
-            new_num = False
+            num_last = num_now
+            num_now = 0
             operation_last = poly[idx]
     # 숫자일 때
     else:
-        if new_num:
-            num_now *= 10
-            num_now += int(poly[idx])
-        else:
-            new_num = True
-            num_last = num_now
-            num_now = int(poly[idx])
+        num_now *= 10
+        num_now += int(poly[idx])
     idx += 1
 
 if output:
