08c1eae630c96b1f6d7830981e031723fd990e02
mintropy <jun2korea@gmail.com>
2021-09-17 22:24:27 +0900

    9935 solving

--- 9000/9935.py
+++ 9000/9935.py
@@ -6,77 +6,53 @@ Link : https://www.acmicpc.net/problem/9935
 import sys
 input = sys.stdin.readline
 
-string = input().strip()
-explode = input().strip()
 
-# 출력이 담기는 리스트
-stack = []
-# 폭발 가능성이 있는 문자열
-danger = []
-# 폭발 문자열의 시작 문자열이 있다면, 해당 인덱스
-last_st = []
-
-# 문자열 길이
-l = len(string)
-# 폭발 문자열 길이
-l_d = len(explode)
-# 지금 추적하고 있는 문자열 인덱스
-idx = 0
-# 폭발 문자열이 의심될 때, 추적하는 인덱스
-danger_idx = 0
-# 지난 문자열 확인할 때
-check = 0
-
-while idx < l:
-    s = string[idx]
+def check_explode(st):
+    global string, explode, string_next, string_before
+    l_ex = len(explode)
+    idx = st
+    idx_ex = 0
     # 폭발 문자열인지 확인
-    if danger_idx:
-        # 폭발 문자열
-        if danger_idx == l_d:
-            danger = []
-            # 이전에 지나친 폭발 문자열 있는지 확인
-            while last_st:
-                last_idx = last_st[-1]
-                idx_now = 0
-                # 위 인덱스부터 stack마지막까지 가능한지 확인
-                for _ in range(last_idx, len(stack)):
-                    if stack[last_idx + idx_now] != explode[idx_now]:
-                        break
-                    idx_now += 1
-                # stack마지막까지 확인했으면 
-                # 추가적으로 문자열 돌면서 확인
-                # 더 확인해야할 문자열 길이
-                check_len = l_d - idx_now - 1
-                if idx + check_len > l_d:
-                    break
-                for _ in range(check_len):
-                    if stack[last_idx + idx_now] != explode[idx_now]:
-                        break
-                    idx_now += 1
-                # 모든 과정을 통과했으면 폭발 문자열
-                # stack의 last_idx까지 pop
-                while len(stack) > last_idx:
-                    stack.pop()
-        # 계속 폭발 문자열이 이어지는지
-        elif explode[danger_idx] == s:
-            danger_idx += 1
-            danger.append(s)
-        # 폭발 문자열이 아니게 되면
-        else:
-            danger_idx = 0
-            stack += danger
-            danger = []
-    else:
-        # 새로운 폭발 문자열이 시작되면
-        if explode[danger_idx] == s:
-            last_st.append(len(stack) - 1)
-            danger_idx += 1
-            danger.append(s)
-        else:
-            stack.append(s)
-    idx += 1
+    while idx_ex < l_ex:
+        if string[idx] != explode[idx_ex]:
+            return False
+        idx_ex += 1
+        if idx != l:
+            idx = string_next[idx]
+        elif idx == l:
+            return False
+    string_next[st] = idx
+    return True
 
-if stack:
-    print(*stack, sep='')
+
+string = [0] + list(input().strip())
+l = len(string)
+# 문자열에서 다음을 지목하는 리스트
+string_next = [i + 1 for i in range(l)]
+explode = list(input().strip())
+
+prob = []
+for i in range(1, l):
+    if string[i] == explode[0]:
+        prob.append([i, False])
+
+# 폭발 문자열이 될 수 있는 시작마다 확인
+for i in range(len(prob)):
+    # 폭발 문자열인지 확인
+    if check_explode(prob[i][0]):
+        prob[i][1] = True
+        # 이전 가능성 중 폭발 문자열이 다시 가능한지
+        for j in range(i - 1, -1, -1):
+            if not prob[j][1] and check_explode(prob[j][0]):
+                prob[j][1] = True
+
+idx = string_next[1]
+ans = ''
+while idx < l:
+    ans += string[idx]
+    idx = string_next[idx]
+
+if ans:
+    print(ans)
 else:
     print('FRULA')
