30ef75a7cc72fc97a058c651a4ba53165d3785e7
mintropy <jun2korea@gmail.com>
2021-09-18 20:46:53 +0900

    9656 solved

--- 9000/9656.py
+++ 9000/9656.py
@@ -9,16 +9,3 @@ if n % 2:
     print('CY')
 else:
     print('SK')
-"""
-if n == 1:
-    print('CY')
-elif n == 2:
-    print('SK')
-else:
-    # 기본값은 상근이, 창영이가 이길때만 변경
-    dp = [['SK', 'SK'] for _ in range(n + 1)]
-    dp[1][0] = 'CY'
-    dp[2][1] = 'CY'
-    for i in range(3, n + 1):
-        
-"""
