069a9b16992870a6f8d788a9f91a2dc3feb57bcf
mintropy <jun2korea@gmail.com>
2021-09-19 19:00:56 +0900

    1022 solving

--- 1000/1000/1022.py
+++ 1000/1000/1022.py
@@ -5,8 +5,6 @@ Link : https://www.acmicpc.net/problem/1022
 
 r1, c1, r2, c2 = map(int, input().split())
 
-grid = [[0] * (c2 - c1 + 1) for _ in range(r2 - r1 + 1)]
-
 diagonal = [[1] for _ in range(4)]
 max_grid = max((r1, r2, c1, c2))
 # 0, 0 기준 오른쪽 위, 왼쪽 위, 왼쪽 아래, 오른쪽 아래
@@ -15,4 +13,9 @@ diagonal[1].append(5)
 diagonal[2].append(7)
 diagonal[3].append(9)
 for _ in range(1, max_grid + 1):
-    pass
+    for i in range(4):
+        diagonal[i].append(diagonal[i][-1] + (diagonal[i][-1] - diagonal[i][-2]) + 8)
+
+grid = [[0] * (c2 - c1 + 1) for _ in range(r2 - r1 + 1)]
+# 가장 윗 줄부터 한줄씩 채우기
+
