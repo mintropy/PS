43da1f0598009019742ba3db1c525883a605b5e9
mintropy <jun2korea@gmail.com>
2021-09-19 13:21:16 +0900

    14268 solving

--- 14000/14268.py
+++ 14000/14268.py
@@ -8,20 +8,27 @@ input = sys.stdin.readline
 
 
 def update(i, w):
-    while i != -1:
-        company[i] += w
-        i = sup[i]
-    return
+    check = [i]
+    idx = 0
+    while idx < len(check):
+        j = check[idx]
+        company[j] += w
+        check += junior[j]
+        idx += 1
 
 
 n, m = map(int ,input().split())
 company = [0] * (n + 1)
+# 각 사원의 상사
 sup = [0] + list(map(int, input().split()))
+# 각 사원의 부하
+junior = [[] for _ in range(n + 1)]
+for i in range(2, n + 1):
+    junior[sup[i]].append(i)
 
 
 for _ in range(m):
     query, *todo = map(int, input().split())
-    
     if query == 1:
         update(*todo)
     else:
