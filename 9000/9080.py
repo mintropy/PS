506e88f49ee5e52ce07aa99e0cf667414e80e999
mintropy <jun2korea@gmail.com>
2021-09-19 22:11:22 +0900

    9080 solving

--- 9000/9080.py
+++ 9000/9080.py
@@ -6,7 +6,81 @@ Link : https://www.acmicpc.net/problem/9080
 import sys
 input = sys.stdin.readline
 
+for _ in range(int(input())):
+    st_time, duration = input().strip().split()
+    # 시간 시간, 분
+    st_h, st_m = map(int, st_time.split(':'))
+    # 시간을 00:00 기준 지난 시간
+    now = st_h * 60 + st_m
+    # 총 사용 시간
+    duration = int(duration)
+    
+    fee = 0
+    while duration:
+        # 만약 남은 시간이 5시간 이하면 시간별 요금 내기
+        if duration <= 60 * 5:
+            if duration % 60:
+                fee += (duration // 60 + 1) * 1000
+            else: 
+                fee += (duration // 60) * 1000
+            break
+        # 제한시간이 더 많이 남아있을 때
+        # 야간이고 남은 시간이 5시간 이상이면 정액제
+        if now >= 22 * 60 or now <= 3 * 60:
+            duration -= (24 + 8) * 60 - now
+            fee += 5000
+            now = 8 * 60
+        # 야간 남은 시간 5시간 미만이고, 주간이면
+        # 21:~~ 까지 사용하기
+        elif now <= 21 * 60:
+            time = 21 - now // 60
+            # 더 사용할 시간이 적으면, 모두 사용
+            if time * 60 >= duration:
+                if duration % 60:
+                    fee += (duration // 60 + 1) * 1000
+                else: 
+                    fee += (duration // 60) * 1000
+                break
+            # time에 해당하는 시간만큼만 사용
+            else:
+                fee += time * 1000
+                duration -= time * 60
+                now += time * 60
+        # 21:~~ 이라면 어떻게 할 지 고민하기
+        else:
+            # 21:00 이면 한시간 진행
+            if now == 21 * 60:
+                if duration > 60:
+                    now += 60
+                    fee += 1000
+                    duration -= 60
+                else:
+                    fee += 1000
+                    break
+            time = 10 * 60 + (22 * 60 - now)
+            # 다음날 08:00 이후까지 하는 경우
+            if duration >= time:
+                now = 8 * 60
+                fee += 1000 + 5000
+                duration -= time
+            # 야간 5시간 이상 하는 경우
+            elif duration - (22 * 60 - now) >= 60 * 5:
+                fee += 1000 + 5000
+                break
+            # 야간 5시간 미만 하는 경우
+            else:
+                if duration % 60:
+                    fee += (duration // 60 + 1) * 1000
+                else: 
+                    fee += (duration // 60) * 1000
+                break
+    print(fee)
+
+
 '''
+import sys
+input = sys.stdin.readline
+
 for _ in range(int(input())):
     st_time, duration = input().strip().split()
     # 시간 시간, 분
