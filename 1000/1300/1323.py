3cab24fb51fb1f5ee5238f1c4c72db024bf3aa7b
mintropy <jun2korea@gmail.com>
2021-09-20 09:43:58 +0900

    1323 solved

--- 1000/1300/1323.py
+++ 1000/1300/1323.py
@@ -5,3 +5,20 @@ Link : https://www.acmicpc.net/problem/1323
 
 n, k = map(int, input().split())
 
+# 1번 반복해 적었을 때 나머지
+r = n % k
+
+count = 1
+remainder = set()
+while True:
+    if r in remainder:
+        print(-1)
+        break
+    elif not r:
+        print(count)
+        break
+    else:
+        remainder.add(r)
+        count += 1
+        r = int(str(r) + str(n))
+        r %= k
