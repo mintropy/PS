8e08e744e1a7a6ce7836f8b9e2f8f2cb1ddc6c30
mintropy <jun2korea@gmail.com>
2021-09-29 10:37:45 +0900

    1783 solved

--- 1000/1700/1783.py
+++ 1000/1700/1783.py
@@ -7,3 +7,18 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
+
+if n == 1:
+    print(1)
+elif n == 2:
+    if m >= 8:
+        print(4)
+    else:
+        print(1 + (m - 1) // 2)
+elif m < 7:
+    if m >= 5:
+        print(4)
+    else:
+        print(m)
+else:
+    print(5 + (m - 7))
