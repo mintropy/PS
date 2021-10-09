da4f93295f8c90d77e4c47fcc8db494272498834
mintropy <jun2korea@gmail.com>
2021-10-09 16:08:41 +0900

    20057 solved

--- 20000/20057.py
+++ 20000/20057.py
@@ -8,14 +8,32 @@ input = sys.stdin.readline
 
 
 def move_dust(field: list, x: int, y: int, d: int):
-    global dx, dy, dust_move
+    global n, dx, dy, dust_move, dust_outside
     # x, y 위치로 d방향으로 이동했을 때, 먼지 이동
+    # 지금 자리의 먼지 양
     dust_now = field[x][y]
+    if dust_now == 0:
+        return
+    dust_now_moved = 0
     field[x][y] = 0
     # 각 이동방향으로 먼지 이동
     # 범위를 벗어나면 벗어나는 먼지로 기록
-    
-    pass
+    for d1, d2, p in dust_move[d]:
+        nx, ny = x + d1, y + d2
+        dust_p = int((dust_now * p) / 100)
+        if dust_p == 0:
+            continue
+        dust_now_moved += dust_p
+        if 0 <= nx < n and 0 <= ny < n:
+            field[nx][ny] += dust_p
+        else:
+            dust_outside += dust_p
+    # 남은 먼지는 d방향으로 한 칸 앞으로
+    nx, ny = x + dx[d], y + dy[d]
+    if 0 <= nx < n and 0 <= ny < n:
+        field[nx][ny] += dust_now - dust_now_moved
+    else:
+        dust_outside += dust_now - dust_now_moved
 
 
 n = int(input())
@@ -51,9 +69,8 @@ for dist in moves:
         # 한 칸 앞으로 진행
         x, y = x + dx[direction], y + dy[direction]
         # 먼지 밀어내기
-        
-        pass
+        move_dust(field, x, y, direction)
     # 진행 방향을 반시계 90도 회전
-    d = (d - 1) % 4
+    direction = (direction - 1) % 4
 
 print(dust_outside)
