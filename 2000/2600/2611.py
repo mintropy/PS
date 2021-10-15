3a233164ebbdfd4a9e20e22e061f835a18f871c3
mintropy <jun2korea@gmail.com>
2021-10-15 13:40:56 +0900

    2611 solved

--- 2000/2600/2611.py
+++ 2000/2600/2611.py
@@ -10,20 +10,40 @@ input = sys.stdin.readline
 
 n = int(input())
 m = int(input())
-roads = [[] for _ in range(n)]
+roads = [[] for _ in range(n + 1)]
+roads_in = [0] * (n + 1)
 for _ in range(m):
     p, q, r = map(int, input().split())
-    roads[p].append[(q, r)]
+    roads[p].append((q, r))
+    roads_in[q] += 1
 
-# i도시 j번재에 도착했을 때, 이전 도시
-dp = [[0] * n for _ in range(n)]
-
+# 도시 1번부터 1번 돌아오기까지 위상 정렬
+# 각 도시로 들어온 도로 개수 확인
+# 위상 정렬
+topological_order = []
 queue = collections.deque([1])
 while queue:
     x = queue.popleft()
-    score_noew = dp[x][0]
-    for y, score in roads[x]:
-        pass
+    topological_order.append(x)
+    for y, _ in roads[x]:
+        roads_in[y] -= 1
+        if roads_in[y] == 0:
+            queue.append(y)
 
+dp = [0] * (n + 1)
+dp_last_point = list(range(n + 1))
+for pos_now in topological_order[:-1]:
+    for pos_next, dist in roads[pos_now]:
+        if dp[pos_next] < dp[pos_now] + dist:
+            dp[pos_next] = dp[pos_now] + dist
+            dp_last_point[pos_next] = pos_now
 
+route = [1]
+now = dp_last_point[1]
+while now != 1:
+    route.append(now)
+    now = dp_last_point[now]
+route.append(1)
 
+print(dp[1])
+print(*route[::-1])
