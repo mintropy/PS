1415e40234deed9ff598e3d18d5e4a995c021262
mintropy <jun2korea@gmail.com>
2021-10-17 13:58:06 +0900

    1941 solved

--- 1000/1900/1941.py
+++ 1000/1900/1941.py
@@ -7,8 +7,8 @@ import sys
 input = sys.stdin.readline
 
 
-def dfs(seven_princess_now: set, possible_seat: set, som_pa_now: int):
-    global students, students_check, possible_seven_princess, dx, dy
+def dfs(possible_seat: set, som_pa_now: int):
+    global students, students_check, possible_seven_princess, seven_princess_now, dx, dy
     if 7 - len(seven_princess_now) + som_pa_now < 4:
         return
     if len(seven_princess_now) == 7:
@@ -24,11 +24,13 @@ def dfs(seven_princess_now: set, possible_seat: set, som_pa_now: int):
             if 0 <= nx < 5 and 0 <= ny < 5:
                 tmp_seat.add((nx, ny))
         students_check[x][y] = True
+        seven_princess_now.append((x, y))
         if students[x][y] == 'S':
-            dfs(seven_princess_now | {(x, y)}, (possible_seat | tmp_seat) - {(x, y)}, som_pa_now + 1)
+            dfs((possible_seat | tmp_seat) - {(x, y)}, som_pa_now + 1)
         else:
-            dfs(seven_princess_now | {(x, y)}, (possible_seat | tmp_seat) - {(x, y)}, som_pa_now)
+            dfs((possible_seat | tmp_seat) - {(x, y)}, som_pa_now)
         students_check[x][y] = False
+        seven_princess_now.pop()
 
 
 students = [input().strip() for _ in range(5)]
@@ -36,6 +38,7 @@ students_check = [[False] * 5 for _ in range(5)]
 
 possible_seven_princess = set()
 dx, dy = (-1, 0, 1, 0), (0, 1, 0, -1)
+seven_princess_now = []
 
 for i in range(5):
     for j in range(5):
@@ -45,8 +48,10 @@ for i in range(5):
                 ni, nj = i + dx[d], j + dy[d]
                 if 0 <= ni < 5 and 0 <= nj < 5:
                     possible_seat.add((ni, nj))
+            seven_princess_now.append((i, j))
             students_check[i][j] = True
-            dfs({(i, j)}, possible_seat, 1)
+            dfs(possible_seat, 1)
+            seven_princess_now.pop()
             students_check[i][j] = False
 
 print(len(possible_seven_princess))
