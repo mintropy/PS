7f9df9555dec409463e5e5bb50316c84f31f2053
mintropy <jun2korea@gmail.com>
2021-10-17 14:09:39 +0900

    1941 solved

--- 1000/1900/1941.py
+++ 1000/1900/1941.py
@@ -7,28 +7,31 @@ import sys
 input = sys.stdin.readline
 
 
-def dfs(possible_seat: set, som_pa_now: int):
+def dfs(group_now: int, som_pa_now: int):
     global students, students_check, possible_seven_princess, seven_princess_now, dx, dy
-    if 7 - len(seven_princess_now) + som_pa_now < 4:
+    # 불가능한 경우 / 추가로 더 모아도 솜파 4명 못만듬
+    if 7 - group_now + som_pa_now < 4:
         return
-    if len(seven_princess_now) == 7:
+    # 7명 모두 탐색
+    if group_now == 7:
         if som_pa_now >= 4:
             possible_seven_princess.add(tuple(sorted(seven_princess_now)))
         return
-    for x, y in possible_seat:
-        if students_check[x][y]:
-            continue
-        tmp_seat = set()
+    # 각 위치마다 가능한자리 따로 구해보기
+    possible_seat = set()
+    for x, y in seven_princess_now:
         for d in range(4):
             nx, ny = x + dx[d], y + dy[d]
-            if 0 <= nx < 5 and 0 <= ny < 5:
-                tmp_seat.add((nx, ny))
+            if 0 <= nx < 5 and 0 <= ny < 5 and not students_check[nx][ny]:
+                possible_seat.add((nx, ny))
+    # 각 가능한 자리 하나씩 추가하며 탐색
+    for x, y in possible_seat:
         students_check[x][y] = True
         seven_princess_now.append((x, y))
         if students[x][y] == 'S':
-            dfs((possible_seat | tmp_seat) - {(x, y)}, som_pa_now + 1)
+            dfs(group_now + 1, som_pa_now + 1)
         else:
-            dfs((possible_seat | tmp_seat) - {(x, y)}, som_pa_now)
+            dfs(group_now + 1, som_pa_now)
         students_check[x][y] = False
         seven_princess_now.pop()
 
@@ -42,17 +45,12 @@ seven_princess_now = []
 
 for i in range(5):
     for j in range(5):
+        # 솜파 한명 기준으로 탐색하면 다시 그 사람 속하는 그룹으로 탐색 필요 ㄴㄴ
         if students[i][j] == 'S':
-            possible_seat = set()
-            for d in range(4):
-                ni, nj = i + dx[d], j + dy[d]
-                if 0 <= ni < 5 and 0 <= nj < 5:
-                    possible_seat.add((ni, nj))
             seven_princess_now.append((i, j))
             students_check[i][j] = True
-            dfs(possible_seat, 1)
+            dfs(1, 1)
             seven_princess_now.pop()
-            students_check[i][j] = False
 
 print(len(possible_seven_princess))
 
