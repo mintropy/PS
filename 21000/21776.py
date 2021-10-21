4544495909bf7de6d71b8a49538d0a070002eaf1
mintropy <jun2korea@gmail.com>
2021-10-21 16:19:09 +0900

    21776 solved

--- 21000/21776.py
+++ 21000/21776.py
@@ -8,6 +8,72 @@ input = sys.stdin.readline
 
 
 def dfs(word_now: str):
+    global n, cards, possible_output, turn, turn_now
+    last_turn = True
+    # 각 사람이 다른 카드를 더 낼 수 있는지 확인
+    for i in range(n):
+        x = turn_now[i]
+        # 해당 사람 모든 행동 다 했을 때
+        if x >= len(turn[i]):
+            continue
+        last_turn = False
+        card_set = cards[turn[i][x]]
+        word_next = calc_card(word_now, card_set)
+        if word_next == 'ERROR':
+            possible_output.add(word_next)
+            continue
+        turn_now[i] += 1
+        dfs(calc_card(word_now, card_set))
+        turn_now[i] -= 1
+    # 마지막일때
+    if last_turn:
+        if word_now:
+            possible_output.add(word_now)
+        else:
+            possible_output.add('EMPTY')
+        return
+
+
+def calc_card(word_now: str, card_set: list) -> str:
+    for cmd, x in card_set:
+        if cmd == 'ADD':
+            word_now += x
+        elif cmd == 'DEL':
+            x = int(x)
+            if x < 0 or x >= len(word_now):
+                return 'ERROR'
+            word_now = word_now[:x] + word_now[x + 1:]
+    return word_now
+
+
+n, c = map(int, input().split())
+turn = []
+for _ in range(n):
+    _, *act = map(int, input().split())
+    turn.append(act)
+
+cards = [[]]
+for _ in range(c):
+    cards_list = list(input().strip().split(','))
+    tmp = []
+    for card in cards_list:
+        tmp.append(tuple(card.split()))
+    cards.append(tmp)
+
+
+perm_list = []
+for i in range(n):
+    perm_list += [i] * len(turn[i])
+
+possible_output = set()
+turn_now = [0] * n
+
+dfs('')
+print(*sorted(possible_output), sep='\n')
+
+
+'''
+def dfs(word_now: str):
     global n, turn, cards, possible_output
     # 다음사람 카드 내는 것 탐색
     turn_left = False
@@ -17,8 +83,10 @@ def dfs(word_now: str):
             continue
         # 아니라면 카드 내기
         turn_left = True
+        # 다음 행동
         next_card = turn[i].pop()
         next_word = calc_card(word_now, cards[next_card])
+        # ERROR 발생시 깊에 재귀 ㄴㄴ
         if next_word == 'ERROR':
             possible_output.add(next_word)
             continue
@@ -26,7 +94,10 @@ def dfs(word_now: str):
         turn[i].append(next_card)
     # 더 낼 수 있는 카드가 없다면
     if not turn_left:
-        possible_output.add(word_now)
+        if not word_now:
+            possible_output.add('EMPTY')
+        else:
+            possible_output.add(word_now)
         return
 
 
@@ -37,7 +108,7 @@ def calc_card(word_now: str, card_set: list) -> str:
             word_now += x
         elif cmd == 'DEL':
             x = int(x)
-            if x >= len(word_now):
+            if x < 0 or x >= len(word_now):
                 return 'ERROR'
             word_now = word_now[:x] + word_now[x + 1:]
     return word_now
@@ -58,3 +129,4 @@ possible_output = set()
 
 dfs('')
 print(*sorted(possible_output), sep='\n')
+'''
