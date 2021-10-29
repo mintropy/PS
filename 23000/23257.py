3a34c06e2dede5c9f135ccc5d7ac26cd97edb1d1
mintropy <jun2korea@gmail.com>
2021-10-29 23:08:24 +0900

    23257 solved

--- 23000/23257.py
+++ 23000/23257.py
@@ -11,6 +11,41 @@ MIIS = lambda: map(int, input().split())
 
 
 n, m = MIIS()
+montly_bong = list(map(lambda x: abs(int(x)), input().split()))
+
+last_montly_bong = set(montly_bong)
+for _ in range(m - 1):
+    last_montly_bong = set(i ^ j for i in montly_bong for j in last_montly_bong)
+
+print(max(last_montly_bong))
+
+
+'''
+# AC
+n, m = MIIS()
+montly_bong = list(MIIS())
+dp = [[False] * 1024 for _ in range(m)]
+# 월봉값을 절댓값으로 저장 & 월봉값 있는 위치에 표시
+for i in range(n):
+    montly_bong[i] = abs(montly_bong[i])
+    dp[0][montly_bong[i]] = True
+
+# 2 ~ m개 선택하는 경우의 수 탐색
+for i in range(1, m):
+    # 각각 위치 탐색
+    for bong in montly_bong:
+        for j in range(1024):
+            dp[i][j ^ bong] |= dp[i - 1][j]
+
+for i in range(1023, -1, -1):
+    if dp[m - 1][i]:
+        print(i)
+        break
+'''
+
+'''
+# TLE
+n, m = MIIS()
 montly_bong = list(MIIS())
 # 월봉값을 절댓값으로 저장
 for i in range(n):
@@ -39,3 +74,4 @@ else:
                     price_next = price_last | price_tmp
             dp[i] = price_next
     print(max(dp))
+'''
