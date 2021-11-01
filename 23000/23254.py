e6d873b5b5aa2c9f5968534f30847a24f73df8c8
mintropy <jun2korea@gmail.com>
2021-11-01 10:55:01 +0900

    23254 solved

--- 23000/23254.py
+++ 23000/23254.py
@@ -3,6 +3,37 @@ Title : 나는 기말고사형 인간이야
 Link : https://www.acmicpc.net/problem/23254
 """
 
+import sys
+input = sys.stdin.readline
+MIIS = lambda: map(int, input().split())
+
+N, M = MIIS()
+grade_now = list(MIIS())
+grade_upgrade = list(MIIS())
+
+# 총 남은 시간
+time_left = 24 * N
+# 한번에 올릴 점수, 시행 횟수, 인덱스
+grade_upgrade_by_idx = []
+for idx, upgrade in enumerate(grade_upgrade):
+    if 100 - grade_now[idx] >= upgrade:
+        grade_upgrade_by_idx.append((upgrade, (100 - grade_now[idx]) // upgrade ,idx))
+    if (100 - grade_now[idx]) % upgrade:
+        grade_upgrade_by_idx.append(((100 - grade_now[idx]) % upgrade, 1, idx))
+grade_upgrade_by_idx.sort(key=lambda x: -x[0])
+
+for upgrade, time, idx in grade_upgrade_by_idx:
+    if time > time_left:
+        grade_now[idx] += upgrade * time_left
+        break
+    else:
+        time_left -= time
+        grade_now[idx] += upgrade * time
+
+print(sum(grade_now))
+
+
+'''
 import heapq
 import sys
 input = sys.stdin.readline
@@ -42,3 +73,4 @@ while grade_upgrade_by_idx and time_left:
                 heapq.heappush(grade_upgrade_by_idx, (-(100 - grade_now[idx]), idx))
 
 print(sum(grade_now))
+'''
