3ddd7e25277fb34e2f1268f45ad4025c497e8798
mintropy <jun2korea@gmail.com>
2021-11-07 12:07:55 +0900

    23039 solved

--- 23000/23039.py
+++ 23000/23039.py
@@ -8,11 +8,24 @@ input = sys.stdin.readline
 
 
 def is_planar_graph(graph: list) -> bool:
-    if 5 in graph[2] and (3 in graph[1] or 5 in graph[1]):
+    # 내부 교차점이 있는지 확인
+    if 5 in graph[2] and (3 in graph[1] or 4 in graph[1]):
         return False
     if 4 in graph[2] and (1 in graph[3] or 5 in graph[3]):
         return False
-    if 3 in graph[2] and (1 in graph[4] or 2 in graph[4]):
+    if 5 in graph[3] and 4 in graph[1]:
+        return False
+    return True
+
+
+def one_point_except_planar(graph: list) -> bool:
+    # 1 ~ 5번점을 하나씩 제외하며 평면 그래프가 되는지 확인
+    # 직접 점을 제거하는 대신, 한 점이 없다면 == 점이 4개라면 내부 교차점은 최대 1개
+    if (4 in graph[2] and 5 in graph[3]) and\
+        (4 in graph[1] and 5 in graph[3]) and\
+        (4 in graph[1] and 5 in graph[2]) and\
+        (3 in graph[1] and 5 in graph[2]) and\
+        (3 in graph[1] and 4 in graph[2]):
         return False
     return True
 
@@ -31,8 +44,10 @@ if N == 10:
 elif N == 1 or is_planar_graph(graph):
     print(0)
 # 한마리 옮겨서 가능
-
-
+# 해당 정점 제외 평면 그래프일 때
+elif one_point_except_planar(graph):
+    print(1)
 # 두마리 올겨서 가능
-
-
+# 나머지 모든 경우 / 3마리 옮겨서 가능 == 2마리 옮겨서 가능
+else:
+    print(2)
