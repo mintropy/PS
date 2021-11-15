fbfdb378c1d61fa6c43425b5546ea675677e5554
mintropy <jun2korea@gmail.com>
2021-11-15 16:20:58 +0900

    1897 solved

--- 1000/1800/1897.py
+++ 1000/1800/1897.py
@@ -15,6 +15,38 @@ for _ in range(int(d)):
         additional_strings[len(string)].append(string)
     else:
         additional_strings[len(string)] = [string]
+additional_strings[3] = [string_0]
 
+max_word_len = 3
+for i in range(4, 81):
+    # 한 글자 추가해서 만들지 못할 때
+    if i not in additional_strings:
+        break
+    possible_string = []
+    # 길이가 i인 단어를 i - 1 길이로 만들 수 있는지
+    for next_word in additional_strings[i]:
+        for word in additional_strings[i - 1]:
+            diff = 0
+            idx0 = idx1 = 0
+            while idx0 < i:
+                if idx1 == i - 1:
+                    diff +=1
+                    break
+                if next_word[idx0] == word[idx1]:
+                    idx0 += 1
+                    idx1 += 1
+                else:
+                    diff += 1
+                    idx0 += 1
+            if diff == 1:
+                possible_string.append(next_word)
+                break
+    # 한 글자 추가해서 만들지 못할 때
+    if not possible_string:
+        break
+    # 한 글자 추가해서 만들 수 있을 떄
+    else:
+        additional_strings[i] = possible_string[::]
+        max_word_len = i
 
-
+print(additional_strings[max_word_len][0])
