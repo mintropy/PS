47fdf83a1a4ce3dcdd21715379aa41c5072fc1ec
mintropy <jun2korea@gmail.com>
2021-11-28 18:27:17 +0900

    16571 solving

---
+++ 16000/16571.py
@@ -0,0 +1,12 @@
+"""
+Title : 알파 틱택토
+Link : https://www.acmicpc.net/problem/16571
+"""
+
+import sys
+input = sys.stdin.readline
+
+TIK_TAK_TOE = [list(map(int, input().split())) for _ in range(3)]
+
+
+
