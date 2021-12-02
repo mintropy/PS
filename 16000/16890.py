dbb11c1d71315b7fdd29d2dbc2e8c0f1c613b756
mintropy <jun2korea@gmail.com>
2021-12-02 18:09:44 +0900

    16890 solving

--- 16000/16890.py
+++ 16000/16890.py
@@ -9,13 +9,34 @@ input = sys.stdin.readline
 
 ko_alp = sorted(list(input().strip()), reverse=True)
 cu_alp = sorted(list(input().strip()))
+l = len(ko_alp)
 
-ans = ''
+if l % 2:
+    ko_alp = ko_alp[l//2:]
+    cu_alp = cu_alp[l//2+1:]
+else:
+    ko_alp = ko_alp[l//2:]
+    cu_alp = cu_alp[l//2:]
 
-for i in range(len(ko_alp)):
+ans_front = ''
+ans_back = ''
+ko_idx = cu_idx = 0
+
+for i in range(l):
     if i % 2:
-        ans += cu_alp.pop()
+        try:
+            cu_alp[-1] >= ko_alp[-1]
+            ans_front += cu_alp.pop()
+        except:
+            ans_back += cu_alp[0]
+            cu_idx += 1
     else:
-        ans += ko_alp.pop()
+        try:
+            ko_alp[-1] <= cu_alp[-1]
+            ans_front += ko_alp.pop()
+        except:
+            ans_back += ko_alp[0]
+            ko_idx += 1
 
-print(ans)
+ans_back = ans_back[::-1]
+print(ans_front + ans_back)
