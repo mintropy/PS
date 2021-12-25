5f22f321c302e5db1a930071ccf859e123e753f8
mintropy <jun2korea@gmail.com>
2021-12-25 09:52:32 +0900

    4781 solved update

--- 4000/4781.py
+++ 4000/4781.py
@@ -8,22 +8,18 @@ input = sys.stdin.readline
 ISS = lambda: input().strip().split()
 
 
-def float_to_int(float_str: str) -> int:
-    return int(float_str[0]) * 100 + int(float_str[2]) * 10 + int(float_str[3])
-
-
 while True:
     N, M = ISS()
     if N == '0' and M == '0.00':
         break
     N = int(N)
-    M = float_to_int(M)
+    M = int(M.replace('.', ''))
     
     dp = [0] * (M + 1)
     for _ in range(N):
         c, p = ISS()
         c = int(c)
-        p = float_to_int(p)
+        p = int(p.replace('.', ''))
         for i in range(p, M + 1):
             if dp[i] < dp[i - p] + c:
                 dp[i] = dp[i - p] + c
