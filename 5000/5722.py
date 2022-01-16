655ec9e844d162ce54eb1d3d160da88257936c7b
mintropy <jun2korea@gmail.com>
2022-01-16 10:12:25 +0900

    5722 solving

---
+++ 5000/5722.py
@@ -0,0 +1,15 @@
+"""
+Title : DNA 부분 수열
+Link : https://www.acmicpc.net/problem/5722
+"""
+
+import sys
+input = sys.stdin.readline
+
+
+while True:
+    x = int(input())
+    if x == 0:
+        break
+    seq1 = input().strip()
+    seq2 = input().strip()
