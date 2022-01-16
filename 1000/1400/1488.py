e3566b91a45b61b770edf8ffcc3d0a335fd2f38e
mintropy <jun2korea@gmail.com>
2022-01-16 09:24:03 +0900

    1488 solving

---
+++ 1000/1400/1488.py
@@ -0,0 +1,13 @@
+"""
+Title : 숌트링
+Link : https://www.acmicpc.net/problem/1488
+"""
+
+import sys
+input = sys.stdin.readline
+
+
+count_A, count_B, max_A, max_B = map(int, input().split())
+max_length = count_A + count_B
+
+
