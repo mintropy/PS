c1811d64710286b4f6fc3ef0a5db2aed390a640c
mintropy <jun2korea@gmail.com>
2022-01-17 20:43:35 +0900

    16206 solving

---
+++ 16000/16206.py
@@ -0,0 +1,15 @@
+"""
+Title : 롤케이크
+Link : https://www.acmicpc.net/problem/16206
+"""
+
+import sys
+input = sys.stdin.readline
+MIIS = lambda: map(int, input().split())
+
+
+N, M = MIIS()
+cakes = list(MIIS())
+
+
+
