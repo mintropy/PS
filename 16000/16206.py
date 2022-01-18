d2b2bb390a7e165ab0fd3493135ea5b29434abd8
mintropy <jun2korea@gmail.com>
2022-01-18 18:49:08 +0900

    16206 solved

--- 16000/16206.py
+++ 16000/16206.py
@@ -11,5 +11,20 @@ MIIS = lambda: map(int, input().split())
 N, M = MIIS()
 cakes = list(MIIS())
 
+count = 0
+additional_count = 0
 
+for cake in sorted(cakes):
+    if M == 0:
+        break
+    if cake % 10 == 0:
+        if cake // 10 - 1 <= M:
+            count += cake // 10
+            M -= cake // 10 - 1
+        else:
+            count += M
+            M = 0
+    else:
+        additional_count += cake // 10
 
+print(count + min(M, additional_count))
