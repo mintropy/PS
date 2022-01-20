8d21794ef1b779dfc5eb56c940e92593a09e35b9
mintropy <jun2korea@gmail.com>
2022-01-20 19:41:15 +0900

    13249 solving

---
+++ 13000/13249.py
@@ -0,0 +1,16 @@
+"""
+Title : 공의 충돌
+Link : https://www.acmicpc.net/problem/13249
+"""
+
+import sys
+input = sys.stdin.readline
+
+
+N = int(input())
+balls = list(map(int, input().split()))
+T = int(input())
+
+
+
+
