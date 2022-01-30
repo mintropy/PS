074e693c6395855ba49eaf38c2cc150c1c0f4522
mintropy <jun2korea@gmail.com>
2022-01-30 19:55:13 +0900

    1111 solving

--- 1000/1100/1111.py
+++ 1000/1100/1111.py
@@ -6,14 +6,21 @@ Link : https://www.acmicpc.net/problem/1111
 import sys
 input = sys.stdin.readline
 
-n = int(input())
+N = int(input())
 seq = list(map(int, input().split()))
 
 
-
-if n == 2:
+if N <= 2:
     print('A')
-elif n == 3:
-    a = (seq[2] - seq[1]) // (seq[1] - seq[0])
+elif N >= 3:
+    a = (seq[1] - seq[2]) // (seq[0] - seq[1])
     b = seq[2] - a * seq[0]
-    
+    possible = True
+    for i in range(1, N - 1):
+        if seq[i] * a + b != seq[i + 1]:
+            possible = False
+            break
+    if possible:
+        print(seq[-1] * a + b)
+    else:
+        print('B')
