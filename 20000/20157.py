da0048a97582982a1e6cabb06f02bbb1e10d0490
mintropy <jun2korea@gmail.com>
2022-01-31 11:49:44 +0900

    20157 solved

--- 20000/20157.py
+++ 20000/20157.py
@@ -3,6 +3,7 @@ Title : 화살을 쏘자!
 Link : https://www.acmicpc.net/problem/20157
 """
 
+from collections import defaultdict
 from math import gcd
 import sys
 input = sys.stdin.readline
@@ -10,22 +11,26 @@ input = sys.stdin.readline
 
 N = int(input())
 
-bloons = {}
+sector = {(True, True): 1, (False, True): 2, (False, False): 3, (True, False): 4}
+bloons = defaultdict(int)
 for _ in range(N):
     x, y = map(int, input().split())
     if x == 0:
-        if 0 in bloons:
-            bloons[0] += 1
+        if y > 0:
+            bloons['x+'] += 1
         else:
-            bloons[0] = 1
+            bloons['x-'] += 1
         continue
+    if y == 0:
+        if x > 0:
+            bloons['y+'] += 1
+        else:
+            bloons['y-'] += 1
     g = gcd(x, y)
     x, y = x // g, y // g
+    s = sector[(x > 0, y > 0)]
     if x < 0:
         x, y = -x, -y
-    if (x, y) in bloons:
-        bloons[(x, y)] += 1
-    else:
-        bloons[(x, y)] = 1
+    bloons[(s, x, y)] += 1
 
 print(max(bloons.values()))
