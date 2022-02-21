a6d72d99ba4e1dad652a9b87610ce7c34b580d9e
mintropy <jun2korea@gmail.com>
2022-02-21 16:52:20 +0900

    1719 solved

--- 1000/1700/1719.py
+++ 1000/1700/1719.py
@@ -9,40 +9,43 @@ input = sys.stdin.readline
 MIIS = lambda: map(int, input().split())
 
 
-def search(dist: list, routes: list, x: int) -> list:
-    heap = [(t, x, x) for x, t in routes[x]]
-    heapq.heapify(heap)
+def search(dist: list, routes: list, N: int, st: int) -> list:
+    heap = [(t, x, x) for x, t in routes[st]]
+    visited = [1_000_000_000] * (N + 1)
+    visited[st] = 0
+    for x, t in routes[st]:
+        visited[x] = t
     while heap:
-        t, y, pos = heapq.heappop(heap)
-        if x != y:
-            if dist[x][y] > 0:
-                continue
-            dist[x][y] = pos
-        for z, et in routes[y]:
-            if dist[x][z] == -1:
-                heap.append((t + et, z, pos))
+        cost, pos, first = heapq.heappop(heap)
+        if visited[pos] < cost:
+            continue
+        dist[st - 1][pos - 1] = first
+        for _pos, _cost in routes[pos]:
+            if cost + _cost < visited[_pos]:
+                heap.append((cost + _cost, _pos, first))
+                visited[_pos] = cost + _cost
         heapq.heapify(heap)
     return dist
 
 
 def solution() -> None:
     N, M = MIIS()
-    routes = [[] for _ in range(N)]
+    routes = [[] for _ in range(N + 1)]
     for _ in range(M):
         x, y, t = MIIS()
-        routes[x - 1].append((y - 1, t))
-        routes[y - 1].append((x - 1, t))
+        routes[x].append((y, t))
+        routes[y].append((x, t))
 
-    dist = [[-1] * N for _ in range(N)]
-    for i in range(N):
-        dist = search(dist, routes, i)
+    dist = [[0] * N for _ in range(N)]
+    for i in range(1, N + 1):
+        dist = search(dist, routes, N, i)
 
     for i in range(N):
         for j in range(N):
             if i == j:
                 print('-', end=' ')
             else:
-                print(dist[i][j] + 1, end=' ')
+                print(dist[i][j], end=' ')
         print()
 
 
