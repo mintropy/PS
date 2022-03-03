74b428500e93f4f266c19f1729a0f77d4935e5d3
mintropy <jun2korea@gmail.com>
2022-03-03 19:55:06 +0900

    24537 solving

---
+++ 24000/24537.py
@@ -0,0 +1,28 @@
+"""
+Title : 약수 계산
+Link : https://www.acmicpc.net/problem/24537
+"""
+
+import sys
+input = sys.stdin.readline
+
+
+if __name__=='__main__':
+    N = int(input())
+    nums = sorted(map(int, input().split()))
+    Q = int(input())
+    
+    is_prime = [True] * 317
+    primes = []
+    for i in range(2, 317):
+        if is_prime[i]:
+            primes.append(i)
+            for j in range(i * 2, 317, i):
+                is_prime[j] = False
+    
+    count = [-1] * 100_001
+    
+    
+    
+    for _ in range(Q):
+        K = int(input())
