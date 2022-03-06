70ab6178a224c587b73857fa342b0fbf2c9b0897
mintropy <jun2korea@gmail.com>
2022-03-06 12:21:29 +0900

    20928 solved

--- 20000/20928.py
+++ 20000/20928.py
@@ -3,6 +3,7 @@ Title : 걷는 건 귀찮아
 Link : https://www.acmicpc.net/problem/20928
 """
 
+from collections import deque
 import sys
 input = sys.stdin.readline
 MIIS = lambda: map(int , input().split())
@@ -13,18 +14,36 @@ if __name__ == '__main__':
     positions = list(MIIS())
     moves = list(MIIS())
 
-    dp = [-1] * (M + 1)
-    count = 0
-    for i in range(positions[0], min(M + 1, positions[0] + moves[0])):
-        dp[i] = 0
-    next_pos = positions[0] + moves[0] + 1
-
-    for idx, pos in enumerate(positions[1:]):
-        if dp[pos] == -1:
+    min_count = N + 1
+    count = [-1] * (N + 1)
+    count[0] = 0
+    right = 0
+    for left, pos in enumerate(positions):
+        if count[left] == -1:
             break
-        if next_pos < pos + moves[idx]:
-            tmp = min(M + 1, pos + moves[idx])
-            for i in range(next_pos, tmp):
-                dp[i] = dp[pos] + 1
-            next_pos = tmp + 1
-    print(dp[M])
+        max_range = moves[left]
+        if M <= pos + max_range:
+            count[-1] = count[left]
+            break
+        for i in range(right + 1, N):
+            next_pos = positions[i]
+            if next_pos > pos + max_range:
+                break
+            count[i] = count[left] + 1
+            right = i
+    print(count[-1])
+
+'''
+Counter Example
+4 10
+1 3 5 7
+4 6 5 2
+
+1 1
+1
+1
+
+5 20
+1 4 8 12 16
+5 8 2 7 4
+'''
