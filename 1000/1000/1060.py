712d1c5f326a2db2f45519ca8529fb89ade30135
mintropy <jun2korea@gmail.com>
2022-03-11 22:32:06 +0900

    1060 solving

---
+++ 1000/1000/1060.py
@@ -0,0 +1,15 @@
+"""
+Title : 좋은 수
+Link : https://www.acmicpc.net/problem/1060
+"""
+
+import sys
+input = sys.stdin.readline
+
+
+if __name__ == '__main__':
+    L = int(input())
+    nums = sorted(map(int, input().split()))
+    N = int(input())
+    
+    
