1ee53c5c09215e58e377a0d8b30fc2d7020c20dc
mintropy <jun2korea@gmail.com>
2022-03-14 23:19:03 +0900

    2338 solved

---
+++ 2000/2300/2338.py
@@ -0,0 +1,13 @@
+"""
+Title : 긴자리 계산
+Link : https://www.acmicpc.net/problem/2338
+"""
+
+import sys
+input = sys.stdin.readline
+
+
+if __name__ == "__main__":
+    A = int(input())
+    B = int(input())
+    print(f'{A + B}\n{A - B}\n{A * B}')
