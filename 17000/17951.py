22e316ee9b198f7c1c42fecf501089b04c10e54e
mintropy <jun2korea@gmail.com>
2022-03-15 19:48:41 +0900

    17951 solved

--- 17000/17951.py
+++ 17000/17951.py
@@ -11,23 +11,20 @@ MIIS = lambda: map(int, input().split())
 if __name__ == "__main__":
     N, K = MIIS()
     correct_counts = tuple(MIIS())
-    left, right = N // K, (sum(correct_counts) // K) + 1
-    ans = right
+    left, right = 0, sum(correct_counts)
+    ans = 0
     while left <= right:
         mid = (left + right) // 2
-        correct_sums = []
         tmp = 0
+        count = 0
         for correct in correct_counts:
             tmp += correct
             if tmp >= mid:
-                correct_sums.append(tmp)
+                count += 1
                 tmp = 0
-        else:
-            if tmp:
-                correct_sums.append(tmp)
-        if len(correct_sums) <= K:
-            ans = min(correct_sums)
-            right = mid - 1
-        else:
+        if count >= K:
             left = mid + 1
+            ans = mid
+        else:
+            right = mid - 1
     print(ans)
