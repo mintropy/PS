8653bb58d62917a1c1c135840dd9cc3703a8a51e
mintropy <jun2korea@gmail.com>
2022-03-16 22:02:23 +0900

    24041 solved

--- 24000/24041.py
+++ 24000/24041.py
@@ -8,16 +8,36 @@ input = sys.stdin.readline
 MIIS = lambda: map(int, input().split())
 
 
-N, G, K = MIIS()
-important_ingredients = []
-not_important_ingredients = []
-for _ in range(N):
-    ingredient = tuple(MIIS())
-    if ingredient[2] == 0:
-        important_ingredients.append(ingredient)
-    else:
-        not_important_ingredients.append(ingredient)
+def calc(time: int, S: int, L: int):
+    if time - L > 0:
+        return S * (time - L)
+    return S
 
 
-
-
+if __name__ == "__main__":
+    N, G, K = MIIS()
+    important_ingredients = []
+    not_important_ingredients = []
+    for _ in range(N):
+        S, L, O = MIIS()
+        if not O:
+            important_ingredients.append((S, L))
+        else:
+            not_important_ingredients.append((S, L))
+    if K >= len(not_important_ingredients):
+        K = len(not_important_ingredients)
+    left, right = 0, 10 ** 10
+    ans = 0
+    while left <= right:
+        mid = (left + right) // 2
+        if K:
+            count = sum([calc(mid, S, L) for S, L in important_ingredients])\
+                + sum(sorted([calc(mid, S, L) for S, L in not_important_ingredients])[:-K])
+        else:
+            count = sum([calc(mid, S, L) for S, L in important_ingredients + not_important_ingredients])
+        if count <= G:
+            ans = mid
+            left = mid + 1
+        else:
+            right = mid - 1
+    print(ans)
