f32ec2bf1e4d67948c4ac5b2b6e3eb62497b4b35
mintropy <jun2korea@gmail.com>
2022-03-22 18:31:12 +0900

    1025 solving

---
+++ 1000/1000/1025.py
@@ -0,0 +1,12 @@
+"""
+Title : 제곱수 찾기
+Link : https://www.acmicpc.net/problem/1025
+"""
+
+import sys
+input = sys.stdin.readline
+
+
+if __name__ == "__main__":
+    N, M = map(int, input().split())
+    numbers = []
