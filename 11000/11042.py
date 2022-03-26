7f66110c52a8780b9fa73f731dcac898f05446b0
mintropy <jun2korea@gmail.com>
2022-03-26 15:10:20 +0900

    11042 solving

---
+++ 11000/11042.py
@@ -0,0 +1,11 @@
+"""
+Title : 이항 계수 4
+Link : https://www.acmicpc.net/problem/11402
+"""
+
+import sys
+input = sys.stdin.readline
+
+
+if __name__ == "__main__":
+    N, K, M = map(int, input().split())
