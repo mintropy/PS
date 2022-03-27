a2eb4141a778f2d41d58e7006e025dca7b097230
mintropy <jun2korea@gmail.com>
2022-03-27 19:47:04 +0900

    15998 solving

--- 15000/15998.py
+++ 15000/15998.py
@@ -10,27 +10,23 @@ input = sys.stdin.readline
 
 if __name__ == "__main__":
     N = int(input())
-    logs = [tuple(map(int, input().split())) for _ in range(N)]
-    auto_deposit = []
-    if logs[0][0] < 0:
-        auto_deposit.append(sum(logs[0]))
-    for i in range(1, N):
-        if logs[i][0] > 0 or logs[i - 1][1] + logs[i][0] >= 0:
-            continue
-        auto_deposit.append(logs[i][1] + -logs[i][0] - logs[i - 1][1])
-    if len(auto_deposit) <= 1:
-        print(1)
+    balance = 0
+    max_in = -1
+    ans = 0
+    for _ in range(N):
+        a, b = map(int, input().split())
+        money = b - a - balance
+        if money > 0:
+            ans = gcd(ans, money)
+            max_in = max(max_in, b)
+        elif money < 0:
+            print(-1)
+            break
+        balance = b
+    if ans > max_in:
+        print(ans if ans else 1)
     else:
-        g = gcd(*auto_deposit)
-        if g == 1:
-            for a, b in logs:
-                if a < 0 and b:
-                    print(-1)
-                    break
-            else:
-                print(1)
-        else:
-            print(g)
+        print(-1)
 
 '''
 2
