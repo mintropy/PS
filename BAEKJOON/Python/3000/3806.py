e04e8dd83aa6d6da0488515ff0e0a1aee6fdfb59
mintropy <jun2korea@gmail.com>
2022-04-17 10:07:51 +0900

    BOJ 3806 solved

--- BAEKJOON/Python/3000/3806.py
+++ BAEKJOON/Python/3000/3806.py
@@ -9,10 +9,32 @@ input = sys.stdin.readline
 
 
 if __name__ == "__main__":
-    for _ in range(int(input())):
+    for TC in range(int(input())):
         S = input().strip()
         T = input().strip()
         changes = defaultdict(int)
         for idx, s in enumerate(S):
             if s != T[idx]:
                 changes[(s, T[idx])] += 1
+        
+        count = 0
+        x = min(changes[('1', '0')], changes[('0', '1')])
+        count += x
+        changes[('1', '0')] -= x
+        changes[('0', '1')] -= x
+        
+        x = min(changes[('1', '0')], changes[('?', '1')])
+        count += x * 2
+        changes[('1', '0')] -= x
+        changes[('?', '1')] -= x
+        
+        if changes[('1', '0')]:
+            count = -1
+        else:
+            count += changes[('0', '1')] + changes[('?', '0')] + changes[('?', '1')]
+        print(f"Case {TC + 1}: {count}")
+
+'''
+(1, 0), (0, 1) 교환 : (1, 1), (0, 0)으로 1번으로 해결
+(1, 0), (?, 1) 교환 : (1, 1), (?, 0)으로 변하므로 2번으로 해결
+'''
