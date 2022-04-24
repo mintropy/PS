c92881214e475264c5ce5414b0f78adc970dc376
mintropy <jun2korea@gmail.com>
2022-04-24 12:19:32 +0900

    BOJ 14238 solved

--- BAEKJOON/Python/14000/14238.py
+++ BAEKJOON/Python/14000/14238.py
@@ -12,24 +12,24 @@ def search(a, b, c, last1, last2):
     A, B, C가 각각 출근한 날 수가 a, b, c이고
     전날 last1, 그 전날 last2가 출근했을 경우
     """
-    global A, B, C, dp, count
-    
-    if a < 0 or b < 0 or c < 0:
-        return False
+    global ans, A, B, C, dp, count
+
     if a == 0 and b == 0 and c == 0:
         return True
     if dp[a][b][c][last1][last2]:
         return False
+
     dp[a][b][c][last1][last2] = True
-    ans[count - a - b - c] = 'A'
-    if search(a - 1, b, c, 0, last1):
-        return True
-    elif last1 != 1:
-        ans[count - a - b - c] = 'B'
+    if a:
+        ans[a + b + c] = "A"
+        if search(a - 1, b, c, 0, last1):
+            return True
+    if b and last1 != 1:
+        ans[a + b + c] = "B"
         if search(a, b - 1, c, 1, last1):
             return True
-    elif last1 != 2 != last2:
-        ans[count - a - b - c] = 'C'
+    if c and last1 != 2 and last2 != 2:
+        ans[a + b + c] = "C"
         if search(a, b, c - 1, 2, last1):
             return True
     return False
@@ -41,10 +41,9 @@ if __name__ == "__main__":
     B = record.count('B')
     C = record.count('C')
     count = len(record)
-    
     dp = [[[[[False] * 3 for _ in range(3)] for _ in range(count + 1)] for _ in range(count + 1)] for _ in range(count + 1)]
-    ans = [''] * (count - 1)
+    ans = [""] * (count + 1)
     if search(A, B, C, 0, 0):
-        print(*ans, sep='')
+        print("".join(ans))
     else:
         print(-1)
