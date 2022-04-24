4c0edcd54d23ee25a2038d6233ccfde4b4c59643
mintropy <jun2korea@gmail.com>
2022-04-24 19:52:20 +0900

    BOJ 24467 solved

--- BAEKJOON/Python/24000/24467.py
+++ BAEKJOON/Python/24000/24467.py
@@ -3,7 +3,6 @@ Title : 혼자 하는 윷놀이
 Link : https://www.acmicpc.net/problem/24467
 """
 
-from distutils import errors
 import sys
 input = sys.stdin.readline
 
@@ -11,52 +10,33 @@ input = sys.stdin.readline
 if __name__ == "__main__":
     pos = 0
     ans = "LOSE"
-    while True:
-        if pos == -1 and ans == "WIN":
+    next_pos = {5: 31, 10: 41, 33: 44}
+    for _ in range(10):
+        if pos == 50 and ans == "WIN":
             break
-        try:
-            move = input().strip()
-            back = move.count("0")
-            if pos == -1:
-                ans = "WIN"
-                break
-            if not back:
-                back = 5
-            if pos == 33:
-                pos = pos + back - 10
-                if pos >= 35:
-                    pos = -1
-            elif pos == 5:
-                pos = 20 + back
-                if pos == 23:
-                    pos = 33
-            elif pos == 10:
-                pos = 30 + back
-            elif 21 <= pos <= 25:
-                pos += back
-                if pos == 23:
-                    pos = 33
-                elif pos > 25:
-                    pos -= 9
-            elif 31 <= pos <= 35:
-                pos += back
-                if pos >= 36:
-                    pos = -1
-            else:
-                pos += back
-                if pos >= 20:
-                    pos = -1
-        except errors:
-            break
+        yuts = input().strip()
+        back_count = yuts.count("0")
+        if back_count == 0:
+            back_count = 5
+        if pos in next_pos:
+            pos = next_pos[pos]
+            back_count -= 1
+        pos += back_count
+        if 35 < pos < 40:
+            pos -= 19
+        elif pos == 46 or pos == 20:
+            pos = 50
+        elif pos > 46 or 20< pos < 30:
+            pos = 50
+            ans = "WIN"
     print(ans)
 
 '''
 10  9  8  7  6  5
-11  31      21  4
-12    32  22    3
-        33
-13    24  34    2
-14  25      35  1
-15 16 17 18 19  0
-
+11  41      31  4
+12    42  32    3
+        33&43
+13    34  44    2
+14  35      45  1
+15 16 17 18 19  0 / 50
 '''
