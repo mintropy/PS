0f1869e066e15524d6821d1088512baf9fa0104c
mintropy <jun2korea@gmail.com>
2022-05-14 13:39:15 +0900

    BOJ 7775 solved

--- BAEKJOON/Python/7000/7775.py
+++ BAEKJOON/Python/7000/7775.py
@@ -7,15 +7,47 @@ import sys
 input = sys.stdin.readline
 
 
+def solve(n, p, k, d):
+    ans = [0] * n
+    if d * (d - 1) // 2 > p:
+        print("Wrong information")
+        return
+    if d == 1:
+        if n == k:
+            if p % k:
+                print("Wrong information")
+                return
+        elif p // k < p % k:
+            print("Wrong information")
+            return
+        else:
+            ans[k] = p % k 
+        for i in range(k):
+            ans[i] = p // k
+    else:
+        for i in range(d):
+            ans[i] = d - i - 1
+    ans[0] += p - sum(ans)
+    print(*ans, sep="\n")
+    return
+
+
 if __name__ == "__main__":
     n, p, k, d = map(int, input().split())
-    if (d * (d + 1)) // 2 + (k - d) > p:
-        print("Wrong information")
-    else:
-        ans = [0] * n
-        for i in range(d):
-            ans[i] = d - i
-        for j in range(d, k):
-            ans[i] = 1
-        ans[0] += p - sum(ans)
-        print(*ans, sep="\n")
+    solve(n, p, k, d)
+
+'''
+5 6 3 1
+
+5 6 4 1
+
+5 6 2 1
+
+5 7 2 1
+
+5 9 2 1
+
+5 6 5 1
+
+5 5 5 1
+'''
