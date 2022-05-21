28fdabf14144699c59a918ad903c1188290bb277
mintropy <jun2korea@gmail.com>
2022-05-21 15:14:15 +0900

    BOJ 16571 solving

--- BAEKJOON/Python/16000/16571.py
+++ BAEKJOON/Python/16000/16571.py
@@ -6,7 +6,58 @@ Link : https://www.acmicpc.net/problem/16571
 import sys
 input = sys.stdin.readline
 
-TIK_TAK_TOE = [list(map(int, input().split())) for _ in range(3)]
+
+def search(TIK_TAK_TOE: list, turn: int, count: int, target_player: int) -> str:
+    if not count:
+        return "D"
+    result = is_tik_tak_toe(TIK_TAK_TOE)
+    if result:
+        if result == target_player:
+            return "W"
+        else:
+            return "L"
+    next_turn = 1 if turn == 2 else 2
+    best_result = "L"
+    for i in range(3):
+        for j in range(3):
+            if not TIK_TAK_TOE[i][j]:
+                TIK_TAK_TOE[i][j] = turn
+                result = search(TIK_TAK_TOE, next_turn, count - 1, target_player)
+                if result == "W":
+                    return result
+                if result == "D":
+                    best_result = result
+                TIK_TAK_TOE[i][j] = 0
+    return best_result
 
 
+def is_tik_tak_toe(TIK_TAK_TOE) -> int:
+    if [1, 1, 1] in TIK_TAK_TOE:
+        return 1
+    if [2, 2, 2] in TIK_TAK_TOE:
+        return 2
+    for i in range(3):
+        if (
+            TIK_TAK_TOE[0][i] == 1
+            and TIK_TAK_TOE[1][i] == 1
+            and TIK_TAK_TOE[2][i] == 1
+        ):
+            return 1
+        if (
+            TIK_TAK_TOE[0][i] == 2
+            and TIK_TAK_TOE[1][i] == 2
+            and TIK_TAK_TOE[2][i] == 2
+        ):
+            return 2
+    return 0
 
+
+if __name__ == "__main__":
+    TIK_TAK_TOE = [list(map(int, input().split())) for _ in range(3)]
+    total_count = one_count = two_count = 0
+    for i in range(3):
+        one_count += TIK_TAK_TOE[i].count(1)
+        two_count += TIK_TAK_TOE[i].count(2)
+        total_count += TIK_TAK_TOE[i].count(0)
+    turn = 1 if one_count == two_count else 2
+    print(search(TIK_TAK_TOE, turn, total_count, turn))
