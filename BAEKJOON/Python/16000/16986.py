4c11e913c9615cc13ed5b9068760c2a81b5ee628
mintropy <jun2korea@gmail.com>
2022-05-29 19:55:31 +0900

    BOJ 16986 solving

--- BAEKJOON/Python/16000/16986.py
+++ BAEKJOON/Python/16000/16986.py
@@ -3,49 +3,11 @@ Title : 인싸들의 가위바위보
 Link : https://www.acmicpc.net/problem/16986
 """
 
-import sys
-imput = sys.stdin.readline
+from itertools import permutations
+from sys import stdin
+
+imput = stdin.readline
 MIIS = lambda: map(int, input().split())
-
-
-def search(match: int, wins: list, player_x: int, player_y: int) -> bool:
-    global N, K, sang_sung, KH, NH
-    if match == 20:
-        return False
-    if player_x == 0:
-        y = KH[match] - 1 if player_y == 1 else MH[match] - 1
-        possible = set(sang_sung[x][y] for x in range(N))
-        if 2 in possible:
-            wins[0] += 1
-            if wins[0] == K:
-                return True
-            next_player = 1 if player_y == 2 else 2
-            if search(match + 1, wins, 0, next_player):
-                return True
-            wins[0] -= 1
-        if 0 in possible or 1 in possible:
-            wins[player_y] += 1
-            if wins[player_y] == K:
-                return False
-            if search(match + 1, wins, 0, player_y):
-                return True
-            wins[player_y] -= 1
-    else:
-        x, y = KH[match] - 1, MH[match] - 1
-        if find_winner(match, x, y):
-            wins[player_x] += 1
-            if wins[player_x] == K:
-                return False
-            if search(match + 1, wins, 0, player_x):
-                return True
-            wins[player_x] -= 1
-        else:
-            wins[player_y] += 1
-            if wins[player_y] == K:
-                return False
-            if search(match + 1, wins, 0, player_y):
-                return True
-            wins[player_y] -= 1
 
 
 def find_winner(x: int, y: int) -> bool:
@@ -55,9 +17,43 @@ def find_winner(x: int, y: int) -> bool:
     return False
 
 
+def simulate(JO: tuple, KH: tuple, MH: tuple) -> bool:
+    global K, sang_sung
+    x, y = 0, 1
+    turn = {0: JO, 1: KH, 2: MH}
+    wins = [0] * 3
+    # JO는 라운드별로 정해진걸 계산하는게 아니라서
+    # 몇번째 자신의 차례인지 기준으로 계산해야함
+    for r in range(20):
+        if find_winner(turn[x][r] - 1, turn[y][r] - 1):
+            wins[x] += 1
+            if wins[x] == K:
+                if x == 0:
+                    return True
+                return False
+            if y == 1:
+                x, y = 0, 2
+            elif y == 2:
+                x, y = 0, 1
+        else:
+            wins[y] += 1
+            if wins[y] == K:
+                return False
+            if x == 0:
+                x, y = 1, 2
+            elif x == 1:
+                x, y = 0, 2
+    return False
+
+
 if __name__ == "__main__":
     N, K = MIIS()
     sang_sung = [tuple(MIIS()) for _ in range(N)]
     KH = tuple(MIIS())
     MH = tuple(MIIS())
-    print(1 if search(0, [0] * 3, 0, 1) else 0)
+    for perm in permutations(range(N), N):
+        if simulate(perm, KH, MH):
+            print(1)
+            break
+    else:
+        print(0)
