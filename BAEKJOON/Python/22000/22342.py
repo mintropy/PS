57fd1f52a94ff9ec12743d1f9d8172c97a7f3efa
mintropy <jun2korea@gmail.com>
2022-05-30 16:02:52 +0900

    BOJ 22342 solved pypy

--- BAEKJOON/Python/22000/22342.py
+++ BAEKJOON/Python/22000/22342.py
@@ -4,32 +4,23 @@ Link : https://www.acmicpc.net/problem/22342
 """
 
 from sys import stdin
+
 input = stdin.readline
-
-
-def get_max_vlaue(output_value: list, i: int, j: int) -> int:
-    global M
-    values = [output_value[i][j - 1]]
-    if i != 0:
-        values.append(output_value[i - 1][j - 1])
-    if i != M - 1:
-        values.append(output_value[i + 1][j - 1])
-    return max(values)
-
-
-def get_max_saved_value(saved_value: list) -> int:
-    return min([line[-1] for line in saved_value])
 
 
 if __name__ == "__main__":
     M, N = map(int, input().split())
-    my_map = list([int(i) for i in input().strip()] for _ in range(M))
-    saved_value = [[0] * N for _ in range(M)]
-    output_value = [[0] * N for _ in range(M)]
-    for i in range(M):
-        output_value[i][0] = my_map[i][0]
-    for j in range(1, N):
-        for i in range(M):
-            saved_value[i][j] = get_max_vlaue(output_value, i, j)
-            output_value[i][j] = saved_value[i][j] + my_map[i][j]
-    print(get_max_saved_value(saved_value))
+    weights = list([int(i) for i in input().strip()] for _ in range(M))
+    output_value = [[0] * (N + 2) for _ in range(M + 2)]
+    saved_value = [[0] * (N + 2) for _ in range(M + 2)]
+    ans = 0
+    for j in range(1, N + 1):
+        for i in range(1, M + 1):
+            saved_value[i][j] = max(
+                output_value[i - 1][j - 1],
+                output_value[i][j - 1],
+                output_value[i + 1][j - 1],
+            )
+            output_value[i][j] = saved_value[i][j] + weights[i - 1][j - 1]
+            ans = max(ans, saved_value[i][j])
+    print(ans)
