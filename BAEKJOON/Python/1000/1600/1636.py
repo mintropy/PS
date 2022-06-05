1f9baf3ffcf251cd6fa25638e63588b187f9b97f
mintropy <jun2korea@gmail.com>
2022-06-05 15:27:29 +0900

    BOJ 1636 solving

---
+++ BAEKJOON/Python/1000/1600/1636.py
@@ -0,0 +1,12 @@
+"""
+Title : 한번 열면 멈출 수 없어
+Link : https://www.acmicpc.net/problem/1636
+"""
+
+from sys import stdin
+
+input = stdin.readline
+
+
+if __name__ == "__main__":
+    N = int(input())
