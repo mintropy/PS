c0409c497bf0d2e3809379fa4c8e1318cef39c17
mintropy <jun2korea@gmail.com>
2022-06-08 20:29:45 +0900

    BOJ 2892 solving

---
+++ BAEKJOON/Python/2000/2800/2892.py
@@ -0,0 +1,13 @@
+"""
+Title : 심심한 준규
+Link : https://www.acmicpc.net/problem/2892
+"""
+
+from sys import stdin
+
+input = stdin.readline
+
+
+if __name__ == "__main__":
+    N = int(input())
+    encrypt_message = list(map(int, input().split()))
