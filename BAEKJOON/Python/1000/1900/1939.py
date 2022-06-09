ac99a43064d1ad2963feb9acac3e8efa0ed69aca
mintropy <jun2korea@gmail.com>
2022-06-09 21:31:19 +0900

    BOJ 1939 solved

--- BAEKJOON/Python/1000/1900/1939.py
+++ BAEKJOON/Python/1000/1900/1939.py
@@ -3,7 +3,7 @@ Title : 중량제한
 Link : https://www.acmicpc.net/problem/1939
 """
 
-from collections import deque
+import heapq
 from sys import stdin
 
 input = stdin.readline
@@ -12,25 +12,25 @@ MIIS = lambda: map(int, input().split())
 
 if __name__ == "__main__":
     N, M = MIIS()
-    bridges = [[] for _ in range(N + 1)]
+    bridges = [{} for _ in range(N + 1)]
     for _ in range(M):
         a, b, c = MIIS()
-        bridges[a].append((b, c))
-        bridges[b].append((a, c))
-    for i in range(1, N + 1):
-        bridges[i].sort(key=lambda x: -x[1])
+        if b not in bridges[a] or c > bridges[a][b]:
+            bridges[a][b] = c
+        if a not in bridges[b] or c > bridges[b][a]:
+            bridges[b][a] = c
 
     st, end = MIIS()
     maximum_weights = [0] * (N + 1)
-    queue = deque([(st, 1_000_000_000)])
-    while queue:
-        pos, weight = queue.popleft()
+    heap = [(-1_000_000_000, st)]
+    while heap:
+        weight, pos = heapq.heappop(heap)
         if maximum_weights[pos]:
             continue
-        maximum_weights[pos] = weight
-        for next_pos, limit in bridges[pos]:
-            next_weight = min(weight, limit)
+        maximum_weights[pos] = -weight
+        for next_pos, limit in bridges[pos].items():
+            next_weight = min(-weight, limit)
             if maximum_weights[next_pos]:
                 continue
-            queue.append((next_pos, next_weight))
+            heapq.heappush(heap, (-next_weight, next_pos))
     print(maximum_weights[end])
