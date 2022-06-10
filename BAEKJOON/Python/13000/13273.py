243519e46bb5f272fac9aff29fd288d87703e8fa
mintropy <jun2korea@gmail.com>
2022-06-10 22:56:04 +0900

    BOJ 13273 solving

--- BAEKJOON/Python/13000/13273.py
+++ BAEKJOON/Python/13000/13273.py
@@ -9,21 +9,57 @@ from sys import stdin
 input = stdin.readline
 
 
-def make_rome(arabic: str) -> str:
+def arabic_to_rome(arabic: str) -> str:
     res = ""
     l = len(arabic)
     for idx, x in enumerate(arabic):
-        pass
+        if x == "4":
+            if l - idx == 3:
+                res += "CD"
+            elif l - idx == 2:
+                res += "XL"
+            elif l - idx == 1:
+                res += "IV"
+        elif x == "9":
+            if l - idx == 3:
+                res += "CM"
+            elif l - idx == 2:
+                res += "XC"
+            elif l - idx == 1:
+                res += "IX"
+        else:
+            x = int(x)
+            if 5 <= x <= 8:
+                if l - idx == 3:
+                    res += "D"
+                elif l - idx == 2:
+                    res += "L"
+                elif l - idx == 1:
+                    res += "V"
+                x -= 5
+            if idx == 0:
+                res += "M" * x
+            elif idx == 1:
+                res += "C" * x
+            elif idx == 2:
+                res += "X" * x
+            elif idx == 3:
+                res += "I" * x
+    return res
+
+
+def rome_to_arabic(rome: str) -> str:
+    res = []
+    idx = 0
+    l = len(rome)
+    return res
 
 
 if __name__ == "__main__":
     for _ in range(int(input())):
-        num = input().strip
-
+        num = input().strip()
         match = re.match("[0-9]*", num)
         if match.group(0) == num:
-
-            print(True)
+            print(arabic_to_rome(num))
         else:
-
-            print(False)
+            print(rome_to_arabic[num])
