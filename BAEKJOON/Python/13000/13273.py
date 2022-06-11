4eec3169249ae287b8e108fbddac45fa6a98106e
mintropy <jun2korea@gmail.com>
2022-06-11 10:17:27 +0900

    BOJ 13273 solved

--- BAEKJOON/Python/13000/13273.py
+++ BAEKJOON/Python/13000/13273.py
@@ -37,21 +37,46 @@ def arabic_to_rome(arabic: str) -> str:
                 elif l - idx == 1:
                     res += "V"
                 x -= 5
-            if idx == 0:
+            if l - idx == 4:
                 res += "M" * x
-            elif idx == 1:
+            elif l - idx == 3:
                 res += "C" * x
-            elif idx == 2:
+            elif l - idx == 2:
                 res += "X" * x
-            elif idx == 3:
+            elif l - idx == 1:
                 res += "I" * x
     return res
 
 
 def rome_to_arabic(rome: str) -> str:
     res = []
-    idx = 0
-    l = len(rome)
+    res.append(rome.count("M") - rome.count("CM"))
+    if "CM" in rome:
+        res.append(9)
+    elif "CD" in rome:
+        res.append(4)
+    else:
+        res.append(rome.count("D") * 5 + rome.count("C") - rome.count("XC"))
+    if "XC" in rome:
+        res.append(9)
+    elif "XL" in rome:
+        res.append(4)
+    else:
+        res.append(rome.count("L") * 5 + rome.count("X") - rome.count("IX"))
+    if "IX" in rome:
+        res.append(9)
+    elif "IV" in rome:
+        res.append(4)
+    else:
+        res.append(rome.count("V") * 5 + rome.count("I"))
+    return get_num(res)
+
+
+def get_num(num_list: list) -> int:
+    res = 0
+    for x in num_list:
+        res *= 10
+        res += x
     return res
 
 
@@ -62,4 +87,4 @@ if __name__ == "__main__":
         if match.group(0) == num:
             print(arabic_to_rome(num))
         else:
-            print(rome_to_arabic[num])
+            print(rome_to_arabic(num))
