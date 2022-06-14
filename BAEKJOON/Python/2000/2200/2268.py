8de05e42a6b68759e2f3b3fa0f53c55e617b06ab
mintropy <jun2korea@gmail.com>
2022-06-14 22:43:09 +0900

    BOJ 2268 solved Python

--- BAEKJOON/Python/2000/2200/2268.py
+++ BAEKJOON/Python/2000/2200/2268.py
@@ -15,6 +15,16 @@ class SegmentTree:
         self.N = N
         self.seq = [0] * (N + 1)
         self.tree = [0] * (1 << (ceil(log2(N)) + 1))
+        self.seq_to_tree = [0] * (N + 1)
+        self.make_link_list(1, 1, self.N)
+
+    def make_link_list(self, node: int, left: int, right: int):
+        if left >= right:
+            self.seq_to_tree[left] = node
+            return
+        mid = (left + right) // 2
+        self.make_link_list(node * 2, left, mid)
+        self.make_link_list(node * 2 + 1, mid + 1, right)
 
     def find_sum(self, node: int, start: int, end: int, left: int, right: int) -> int:
         if right < start or end < left:
@@ -26,15 +36,13 @@ class SegmentTree:
             node * 2 + 1, mid + 1, end, left, right
         )
 
-    def update(self, node: int, start: int, end: int, index: int, diff: int) -> None:
-        if index < start or index > end:
-            return
-        self.tree[node] += diff
-        if start == end:
-            return
-        mid = (start + end) // 2
-        self.update(node * 2, start, mid, index, diff)
-        self.update(node * 2 + 1, mid + 1, end, index, diff)
+    def update(self, node: int, value: int) -> None:
+        node = self.seq_to_tree[node]
+        self.tree[node] = value
+        node //= 2
+        while node:
+            self.tree[node] = self.tree[node * 2] + self.tree[node * 2 + 1]
+            node //= 2
 
 
 if __name__ == "__main__":
@@ -45,6 +53,6 @@ if __name__ == "__main__":
         if not cmd:
             if x > y:
                 x, y = y, x
-            print(segment_tree.find_sum(1, 0, N - 1, x, y))
+            print(segment_tree.find_sum(1, 1, N, x, y))
         else:
-            segment_tree.update(1, 0, N - 1, x, y - segment_tree.seq[x])
+            segment_tree.update(x, y)
