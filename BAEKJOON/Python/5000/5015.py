265472c9e07fd755b213e293447f23e7b130d1c9
mintropy <jun2korea@gmail.com>
2022-06-20 16:39:13 +0900

    BOJ 5015 solved Python

--- BAEKJOON/Python/5000/5015.py
+++ BAEKJOON/Python/5000/5015.py
@@ -8,39 +8,6 @@ from sys import stdin
 input = stdin.readline
 
 
-def search(pattern_idx: int, file_name_idx: int) -> bool:
-    global pattern_length, file_name_length, pattern, file_name, check
-    if pattern_idx == pattern_length and file_name_idx == file_name_length:
-        return True
-    if pattern_idx == pattern_length:
-        return False
-    if file_name_idx == file_name_length:
-        if pattern[pattern_idx] != "*":
-            return False
-        else:
-            if search(pattern_idx + 1, file_name_idx):
-                return True
-            return False
-    if check[pattern_idx][file_name_idx]:
-        return check[pattern_idx][file_name_idx]
-    if pattern[pattern_idx] == "*":
-        result = False
-        if search(pattern_idx + 1, file_name_idx):
-            result = True
-        if search(pattern_idx, file_name_idx + 1):
-            result = True
-        if search(pattern_idx + 1, file_name_idx + 1):
-            result = True
-        check[pattern_idx][file_name_idx] = result
-        return result
-    else:
-        if pattern[pattern_idx] == file_name[file_name_idx]:
-            if search(pattern_idx + 1, file_name_idx + 1):
-                check[pattern_idx][file_name_idx] = True
-                return True
-        return False
-
-
 if __name__ == "__main__":
     pattern = input().strip()
     pattern_length = len(pattern)
@@ -48,7 +15,22 @@ if __name__ == "__main__":
     for _ in range(int(input())):
         file_name = input().strip()
         file_name_length = len(file_name)
-        check = [[False] * file_name_length for _ in range(pattern_length)]
-        if search(0, 0):
-            answer += f"{file_name}\n"
+        file_name_idx = 0
+        for pattern_idx, p in enumerate(pattern):
+            if p == "*":
+                if pattern_idx == pattern_length - 1:
+                    continue
+                if pattern[pattern_idx + 1] == "*":
+                    continue
+                next_pattern = pattern[pattern_idx + 1 :].split("*")[0]
+                file_name_idx = file_name.find(next_pattern, file_name_idx)
+                if file_name_idx == -1:
+                    break
+                continue
+            if p != file_name[file_name_idx]:
+                break
+            file_name_idx += 1
+        else:
+            if pattern[-1] == "*" or pattern[-1] == file_name[-1]:
+                answer += f"{file_name}\n"
     print(answer)
