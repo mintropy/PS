1f3a1464edaeacabd6a0b30f490afbacb15b770b
mintropy <jun2korea@gmail.com>
2022-06-23 19:16:25 +0900

    BOJ 14427 solved Python

--- BAEKJOON/Python/14000/14427.py
+++ BAEKJOON/Python/14000/14427.py
@@ -14,7 +14,7 @@ MIIS = lambda: map(int, input().split())
 class SegmentTree:
     def __init__(self, N: int, seq: list[int]) -> None:
         self.N = N
-        self.seq = seq
+        self.seq = [0] + seq
         self.tree = [0] * (1 << (ceil(log2(N)) + 1))
         self.tree_idx = [0] * (N + 1)
         self.find_tree_idx(1, 1, self.N)
@@ -30,7 +30,18 @@ class SegmentTree:
 
     def init_tree(self) -> None:
         for i, x in enumerate(self.tree_idx):
-            self.tree[x] = self.seq[i]
+            self.tree[x] = i
+        for i in range(max(self.tree_idx[1:]) - 1, 0, -1):
+            if i * 2 + 1 > len(self.tree):
+                continue
+            left = self.seq[self.tree[i * 2]]
+            right = self.seq[self.tree[i * 2 + 1]]
+            if not left * right:
+                continue
+            if left > right:
+                self.tree[i] = self.tree[i * 2 + 1]
+            else:
+                self.tree[i] = self.tree[i * 2]
 
     def find_sum(self, node: int, start: int, end: int, left: int, right: int) -> int:
         if right < start or end < left:
@@ -43,21 +54,28 @@ class SegmentTree:
         return l_sum + r_sum
 
     def update(self, node: int, value: int) -> None:
-        node = self.tree_idx[node]
-        self.tree[node] = value
-        node //= 2
-        while node:
-            self.tree[node] = self.tree[node * 2] + self.tree[node * 2 + 1]
-            node //= 2
+        self.seq[node] = value
+        tree_node = self.tree_idx[node]
+        self.tree[tree_node] = node
+        tree_node //= 2
+        while tree_node:
+            left = self.seq[self.tree[tree_node * 2]]
+            right = self.seq[self.tree[tree_node * 2 + 1]]
+            if left > right:
+                self.tree[tree_node] = self.tree[tree_node * 2 + 1]
+            else:
+                self.tree[tree_node] = self.tree[tree_node * 2]
+            tree_node //= 2
 
 
 if __name__ == "__main__":
     N = II()
     seq = list(MIIS())
-    segment_tree = SegmentTree()
+    segment_tree = SegmentTree(N, seq)
     for _ in range(II()):
         cmd, *nums = MIIS()
         if cmd == 1:
-            pass
+            i, v = nums
+            segment_tree.update(i, v)
         else:
-            pass
+            print(segment_tree.tree[1])
