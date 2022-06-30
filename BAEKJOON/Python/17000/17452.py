8b4047a61f0478bfd5a5917fedf47462f16d1ed9
mintropy <jun2korea@gmail.com>
2022-06-30 22:09:36 +0900

    BOJ 17452 solving

---
+++ BAEKJOON/Python/17000/17452.py
@@ -0,0 +1,26 @@
+"""
+Title : Chritmalo.win
+Link : https://www.acmicpc.net/problem/17452
+"""
+
+from sys import stdin
+input = stdin.readline
+
+
+if __name__ == "__main__":
+    N = int(input())
+    
+
+
+"""
+Ki-pop 영풍
+제목
+    단어 두개 선택,
+    공통된 알파벳을 하나 선택
+    앞에 올 단어에서 그 알파벳 뒤의 문자 지우고
+    뒤에 올 단어에서 알파벳 앞의 문자들을 지운후
+    단어를 합쳐 제목
+christmas & halloween
+    >> 공통 a
+    >> christma + alloween
+"""
