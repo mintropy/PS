dab7bcf55782eb5424c18598e06c5e0fd18f60a8
mintropy <jun2korea@gmail.com>
2022-07-11 13:41:41 +0900

    BOJ 5547 solved Pyhotn

--- BAEKJOON/Python/5000/5500/5547.py
+++ BAEKJOON/Python/5000/5500/5547.py
@@ -3,26 +3,47 @@ Title : 일루미네이션
 Link : https://www.acmicpc.net/problem/5547
 """
 
+from collections import deque
 from sys import stdin
 
 input = stdin.readline
 MIIS = lambda: map(int, input().split())
 
 
+def count_walls(x: int, y: int) -> int:
+    global walls, delta
+    count = 0
+    for dx, dy in delta[x % 2]:
+        nx, ny = x + dx, y + dy
+        if 0 <= nx <= H + 1 and 0 <= ny <= W + 1:
+            if walls[nx][ny]:
+                count += 1
+    return count
+
+
 if __name__ == "__main__":
     W, H = MIIS()
-    walls = [list(MIIS()) for _ in range(H)]
-    visited = [[False] * W for _ in range(H)]
-    delta = {
-        "hexagon": [(0, -1), (1, 0), (0, 1), (-1, 1), (-1, 0), (-1, -1)],
-        "vertex_up": [(0, -1), (-1, -1), (1, 1)],
-        "vertax_down": [(0, 1), (-1, -1), (1, -1)],
-    }
-    dist = 0
-    for i in range(H):
-        for j in range(W):
-            if not walls[i][j] or visited[i][j]:
-                continue
-            st = (i, j)
-            
-    print(dist)
+    walls = (
+        [[0] * (W + 2)] + [[0] + list(MIIS()) + [0] for _ in range(H)] + [[0] * (W + 2)]
+    )
+    visited = [[False] * (W + 2) for _ in range(H + 2)]
+    count = [[0] * (W + 2) for _ in range(H + 2)]
+    delta = [
+        [(-1, 0), (0, 1), (1, 0), (1, -1), (0, -1), (-1, -1)],
+        [(-1, 1), (0, 1), (1, 1), (1, 0), (0, -1), (-1, 0)],
+    ]
+    ans = 0
+    queue = deque([(0, 0)])
+    while queue:
+        x, y = queue.popleft()
+        if visited[x][y]:
+            continue
+        visited[x][y] = True
+        ans += count_walls(x, y)
+        count[x][y] = count_walls(x, y)
+        for dx, dy in delta[x % 2]:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx <= H + 1 and 0 <= ny <= W + 1:
+                if not walls[nx][ny]:
+                    queue.append((nx, ny))
+    print(ans)
