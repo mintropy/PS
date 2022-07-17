f5ce922af979870d173ef2d71298ccea9b4ac1df
mintropy <jun2korea@gmail.com>
2022-07-18 08:57:24 +0900

    BOJ 13258 solving

---
+++ BAEKJOON/Python/13000/13258.py
@@ -0,0 +1,18 @@
+"""
+Title : 복권 + 은행
+Link : https://www.acmicpc.net/problem/13258
+"""
+
+from sys import stdin
+
+input = stdin.readline
+II = lambda: int(input())
+
+
+if __name__ == "__main__":
+    N = II()
+    accounts = list(map(int, input().split()))
+    J = II()
+    C = II()
+    
+    
