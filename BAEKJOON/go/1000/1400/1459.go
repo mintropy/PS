01daf415da328a1a6d960b79202861906de20349
mintropy <jun2korea@gmail.com>
2022-07-23 20:20:28 +0900

    BOJ 1459 solved Go

--- BAEKJOON/go/1000/1400/1459.go
+++ BAEKJOON/go/1000/1400/1459.go
@@ -33,7 +33,8 @@ func main() {
 			min, max = Y, X
 		} else {
 			min, max = X, Y
-		}		dist += min * S
+		}		
+		dist += min * S
 		if S < W {
 			dist += int((max-min)/2) * 2 * S
 			dist += ((max - min) % 2) * W
