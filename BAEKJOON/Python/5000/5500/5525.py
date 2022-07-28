0da9d84691f377b0f29910607509438cad7779a6
mintropy <jun2korea@gmail.com>
2022-07-28 11:22:29 +0900

    BOJ 5525 solved Python

--- BAEKJOON/Python/5000/5500/5525.py
+++ BAEKJOON/Python/5000/5500/5525.py
@@ -1,24 +1,42 @@
-'''
+"""
 Title : IOIOI
 Link : https://www.acmicpc.net/problem/5525
-'''
+"""
 
-import sys
+from sys import stdin
 
-input = sys.stdin.readline
-# 찾는 문자열 P 길이, 문자열 S 길이
-n, m = int(input()), int(input())
-s = str(input().strip())
-# 문자열 s에서 p문자열의 시작, 끝을 나타내는 인덱스
-st, end = -1, -1
-# 지금 보고 있는 인덱스
-idx = 0
-# 찾은 문자열 개수
-count = 0
+input = stdin.readline
 
-for i in range(m - (2 * n) - 1):
-    if s[i] == 'I':
-        if s[i + 1:i + 2 * n + 1] == 'OI' * n:
-            count += 1
 
-print(count)
+def get_count(N: int, left: int, right: int) -> int:
+    o_count = (right - left) // 2
+    if o_count < N:
+        return 0
+    return o_count - N + 1
+
+
+if __name__ == "__main__":
+    n, m = int(input()), int(input())
+    s = str(input().strip())
+
+    count = 0
+    left, right = -1, -1
+    idx = 0
+    while True:
+        if idx == m:
+            count += get_count(n, left, right)
+            break
+        if left == right:
+            if s[idx] == "I":
+                left = idx
+            idx += 1
+        else:
+            if idx == m - 1:
+                idx += 1
+            elif s[idx : idx + 2] == "OI":
+                right = idx + 1
+                idx += 2
+            else:
+                count += get_count(n, left, right)
+                left = right = idx - 1
+    print(count)
