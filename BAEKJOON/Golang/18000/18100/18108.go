0afe515fa2da5fca6f7e40287babb6808b3f40c8
mintropy <jun2korea@gmail.com>
2022-07-31 16:52:45 +0900

    BOJ 18108 solved Python Go

---
+++ BAEKJOON/Golang/18000/18100/18108.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var y int
+	fmt.Scanln(&y)
+	fmt.Println(y - 543)
+}
---
+++ BAEKJOON/Python/18000/18108.py
@@ -0,0 +1,12 @@
+"""
+Title : 1998년생인 내가 태국에서는 2541년생?!
+Link : https://www.acmicpc.net/problem/18108
+"""
+
+from sys import stdin
+input = stdin.readline
+
+
+if __name__ == "__main__":
+    y = int(input())
+    print(y - 543)
