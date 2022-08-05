4cdf9017a22c17f097f97284287bf0ca262dd52c
mintropy <jun2korea@gmail.com>
2022-08-05 16:09:37 +0900

    BOJ 13258 solved Python

--- BAEKJOON/Python/13000/13258.py
+++ BAEKJOON/Python/13000/13258.py
@@ -9,10 +9,17 @@ input = stdin.readline
 II = lambda: int(input())
 
 
+def calc_next(now: float, win_prob: float, prize: int) -> float:
+    win_money = now + prize
+    return win_money * win_prob + now * (1 - win_prob)
+
+
 if __name__ == "__main__":
     N = II()
     accounts = list(map(int, input().split()))
     J = II()
     C = II()
-    
-    
+    for _ in range(C):
+        total_accounts = sum(accounts)
+        accounts = [calc_next(now, now / total_accounts, J) for now in accounts]
+    print(accounts[0])
