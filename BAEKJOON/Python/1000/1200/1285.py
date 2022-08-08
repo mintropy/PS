74b45e6998a9a5746dcec68a38dafa370198a8fc
mintropy <jun2korea@gmail.com>
2022-08-08 12:24:37 +0900

    BOJ 1285 solving

---
+++ BAEKJOON/Python/1000/1200/1285.py
@@ -0,0 +1,13 @@
+"""
+Title : 동전 뒤집기
+Link : https://www.acmicpc.net/problem/1285
+"""
+
+from sys import stdin
+
+input = stdin.readline
+
+
+if __name__ == "__main__":
+    N = int(input())
+    coins = []
