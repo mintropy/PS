adeb56d951a46c742d30d0d4bfca7c097ee4c423
mintropy <jun2korea@gmail.com>
2022-08-16 09:00:12 +0900

    BOJ 16491 solved Pyhthon

--- BAEKJOON/Python/16000/16400/16491.py
+++ BAEKJOON/Python/16000/16400/16491.py
@@ -10,6 +10,11 @@ input = stdin.readline
 MIIS = lambda: map(int, input().split())
 
 
+def is_cross(x1, y1, x2, y2, x3, y3, x4, y4) -> bool:
+    ccw1 = (x1, y1, x2, y2, x3, y3)
+    ccw2 = (x1, y1, x2, y2, x4, y4)
+
+
 def get_ccw(x1, y1, x2, y2, a, b) -> int:
     ccw = (x2 - x1) * (b - y1) - (y2 - y1) * (a - x1)
     if ccw > 0:
@@ -28,9 +33,7 @@ if __name__ == "__main__":
             (x1, y1), (x2, y2) = robots[i], shelters[perm[i]]
             for j in range(i + 1, N):
                 (x3, y3), (x4, y4) = robots[j], shelters[perm[j]]
-                ccw1 = get_ccw(x1, y1, x2, y2, x3, y3)
-                ccw2 = get_ccw(x1, y1, x2, y2, x4, y4)
-                if ccw1 >= 0 and ccw2 >= 0:
+                if is_cross(x1, y2, x2, y2, x3, y3, x4, y4):
                     break
             else:
                 continue
