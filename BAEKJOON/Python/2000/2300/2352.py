fd6768355b45717add8ced85e13458e1bf1d89a3
mintropy <jun2korea@gmail.com>
2022-08-17 09:40:50 +0900

    BOJ 2352 solving

---
+++ BAEKJOON/Python/2000/2300/2352.py
@@ -0,0 +1,18 @@
+"""
+Title : 반도체 설계
+Link : https://www.acmicpc.net/problem/2352
+"""
+
+from sys import stdin
+
+input = stdin.readline
+
+
+def bin_search(N: int, seq: list[int]) -> int:
+    pass
+
+
+if __name__ == "__main__":
+    N = int(input())
+    ports = list(map(int, input().split()))
+    
