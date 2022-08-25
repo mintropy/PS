adffdc27b38f8f170c29a675e1e3a4ef3787b6ff
mintropy <jun2korea@gmail.com>
2022-08-25 22:44:24 +0900

    BOJ 15711 solving

---
+++ BAEKJOON/Python/15000/15711.py
@@ -0,0 +1,13 @@
+"""
+Title : 환상의 짝궁
+Link : https://www.acmicpc.net/problem/15711
+"""
+
+from sys import stdin
+
+input = stdin.readable
+
+
+if __name__ == "__main__":
+    for _ in range(int(input())):
+        A, B = map(int, input().split())
