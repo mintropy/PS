5fe75175ccdfcae5381209256d1130df3c1596cd
mintropy <jun2korea@gmail.com>
2022-08-27 17:05:37 +0900

    BOJ 2842 update

--- BAEKJOON/Python/2000/2800/2842.py
+++ BAEKJOON/Python/2000/2800/2842.py
@@ -11,12 +11,15 @@ input = stdin.readline
 
 def search(high: int, low: int) -> bool:
     queue = deque([post])
+    houses_check = 0
     visited = [[False] * N for _ in range(N)]
     while queue:
         x, y = queue.popleft()
         if visited[x][y]:
             continue
         visited[x][y] = True
+        if town[x][y] == "K":
+            houses_check += 1
         for dx, dy in delta:
             nx, ny = x + dx, y + dy
             if 0 <= nx < N and 0 <= ny < N:
@@ -24,11 +27,6 @@ def search(high: int, low: int) -> bool:
                     continue
                 if low <= heights_map[nx][ny] <= high:
                     queue.append((nx, ny))
-    houses_check = 0
-    for i in range(N):
-        for j in range(N):
-            if visited[i][j] and town[i][j] == "K":
-                houses_check += 1
     if houses_check == houses:
         return True
     return False
@@ -55,21 +53,19 @@ if __name__ == "__main__":
             elif x == "K":
                 houses += 1
                 possible_heights.append(h)
-    house_min_heights, house_max_heights = min(possible_heights), max(possible_heights)
+    house_min_index = heights.index(min(possible_heights))
+    house_max_index = heights.index(max(possible_heights))
 
-    left = right = 0
+    left, right = 0, house_max_index
     ans = 1_000_000
     delta = [(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)]
     while right < len(heights):
-        while left < len(heights):
+        while left <= house_min_index:
             high, low = heights[right], heights[left]
-            if low <= heights_map[post[0]][post[1]] <= high:
-                if search(high, low):
-                    left += 1
-                    if ans > high - low:
-                        ans = high - low
-                else:
-                    break
+            if search(high, low):
+                left += 1
+                if ans > high - low:
+                    ans = high - low
             else:
                 break
         right += 1
