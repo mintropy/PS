cc422545cc9ff24fa900ecdf2812eb1b7ec36e22
mintropy <jun2korea@gmail.com>
2022-08-28 11:01:18 +0900

    BOJ 10255 solving Python WA

--- BAEKJOON/Python/10000/10200/10255.py
+++ BAEKJOON/Python/10000/10200/10255.py
@@ -3,7 +3,6 @@ Title : 교차점
 Link : https://www.acmicpc.net/problem/10255
 """
 
-from re import L
 from sys import stdin
 
 input = stdin.readline
@@ -15,39 +14,29 @@ def line_check(X, Y, A, B) -> int:
     C, D = (A[0], B[1]), (B[0], A[1])
     check_points = {A: False, B: False, C: False, D: False}
     for K in A, B, C, D:
-        if check_vertax(X, Y, K):
+        if (not get_ccw(X, Y, K)) and X <= K <= Y:
             counts += 1
             check_points[K] = True
     lines = ((A, C), (C, B), (D, B), (A, D))
+    if counts == 2:
+        for R1, R2 in lines:
+            if check_points[R1] and check_points[R2]:
+                return 4
+        else:
+            return 2
     for R1, R2 in lines:
-        if check_points[R1] and check_points[R2]:
-            return 4
         tmp1 = get_ccw(X, Y, R1) * get_ccw(X, Y, R2)
         tmp2 = get_ccw(R1, R2, X) * get_ccw(R1, R2, Y)
         if not tmp1 and not tmp2:
-            if X <= R2 and Y >= R1:
-                if is_one_line(R1, R2, X, Y):
-                    return 4
-                if check_points[R2] and R2 in (X, Y):
-                    continue
-                if check_points[R1] and R1 in (X, Y):
-                    continue
-                counts += 1
-        elif tmp1 <= 0 and tmp2 <= 0:
-            point = get_point(X, Y, R1, R2)
-            if check_points[R1] and point == R1:
+            if (R1 == X or R1 == Y) or (R2 == X or R2 == Y):
                 continue
-            if check_points[R2] and point == R2:
+            if is_one_line(R1, R2, X, Y):
+                return 4
+        elif tmp1 <= 0 and tmp2 <= 0:
+            if check_points[R1] or check_points[R2]:
                 continue
             counts += 1
     return counts
-
-
-def check_vertax(X, Y, C) -> bool:
-    if (Y[0] - X[0]) * (C[1] - X[1]) == (Y[1] - X[1]) * (C[0] - X[0]):
-        if X <= C <= Y:
-            return True
-    return False
 
 
 def get_ccw(A, B, C) -> int:
@@ -56,27 +45,9 @@ def get_ccw(A, B, C) -> int:
 
 
 def is_one_line(A, B, X, Y) -> bool:
-    if (A[1] - B[1]) * (X[0] - Y[0]) == (A[0] - B[0]) * (X[1] - Y[1]):
-        if X <= A < Y <= B or A <= X < B <= Y:
-            return True
+    if X <= A < Y <= B or A <= X < B <= Y or X <= A < B <= Y or A <= X < Y <= B:
+        return True
     return False
-
-
-def get_point(A, B, C, D) -> tuple[int]:
-    x1, y2 = A
-    x2, y2 = B
-    x3, y3 = C
-    x4, y4 = D
-    m = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
-    if m:
-        x = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / m
-        y = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / m
-        return (x, y)
-    else:
-        if B == C and A <= C:
-            return B
-        if A == B and C <= A:
-            return A
 
 
 if __name__ == "__main__":
