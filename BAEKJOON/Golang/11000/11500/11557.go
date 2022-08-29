4977410086e83b72cbaa23dc3e7cb214d9110e76
mintropy <jun2korea@gmail.com>
2022-08-29 14:07:31 +0900

    BOJ 11557 solved Go

--- BAEKJOON/Golang/11000/11500/11557.go
+++ BAEKJOON/Golang/11000/11500/11557.go
@@ -4,18 +4,18 @@ import "fmt"
 
 func main() {
 	var t int
-	fmt.Scanf("%d", t)
+	fmt.Scanf("%d", &t)
 
 	for i := 0; i < t; i++ {
 		var n int
-		fmt.Scanf("%d", n)
+		fmt.Scanf("%d", &n)
 		var name string
 		var maxAlcohol int
 		maxAlcohol = -1
 		for j := 0; j < n; j++ {
 			var n string
 			var a int
-			fmt.Scanf("%s %d", n, a)
+			fmt.Scanf("%s %d", &n, &a)
 			if maxAlcohol < a {
 				name, maxAlcohol = n, a
 			}
