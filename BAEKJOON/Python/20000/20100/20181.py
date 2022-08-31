8378b2bcb8c0fc0939b56b202f0153dcc93a56d6
mintropy <jun2korea@gmail.com>
2022-08-31 13:36:39 +0900

    BOJ 20181 solved Python

--- BAEKJOON/Python/20000/20100/20181.py
+++ BAEKJOON/Python/20000/20100/20181.py
@@ -9,5 +9,27 @@ input = stdin.readline
 MIIS = lambda: map(int, input().split())
 
 
-if __name__ == "__maim__":
+if __name__ == "__main__":
     N, K = MIIS()
+    feeds = list(MIIS()) + [0]
+    feeds_status = [[0, 0] for _ in range(N)]
+    left = right = 0
+    feed_sum = feeds[0]
+    while right < N:
+        if feed_sum >= K:
+            feeds_status[left] = [right, feed_sum]
+            feed_sum -= feeds[left]
+            left += 1
+            if right < left:
+                right = left
+                feed_sum += feeds[right]
+        else:
+            right += 1
+            feed_sum += feeds[right]
+    dp = [0] * (N + 1)
+    for i in range(N - 1, -1, -1):
+        if feeds_status == [0, 0]:
+            continue
+        idx, s = feeds_status[i]
+        dp[i] = max(dp[i + 1], dp[idx + 1] + s - K)
+    print(dp[0])
