ee2e365b03f05f19ade30b67c0626bf1fb3cfaba
mintropy <jun2korea@gmail.com>
2022-08-31 09:03:12 +0900

    BOJ 20181 solved Python

---
+++ BAEKJOON/Python/20000/20100/20181.py
@@ -0,0 +1,13 @@
+"""
+Title : 꿈틀꿈틀 호석 애벌레 - 효율성
+Link : https://www.acmicpc.net/problem/20181
+"""
+
+from sys import stdin
+
+input = stdin.readline
+MIIS = lambda: map(int, input().split())
+
+
+if __name__ == "__maim__":
+    N, K = MIIS()
