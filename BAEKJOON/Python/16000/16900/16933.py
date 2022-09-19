53fb03dd8fbbdd7dc763b75a06e098480cb4cbfe
mintropy <jun2korea@gmail.com>
2022-09-19 11:53:42 +0900

    BOJ 16933 solving WA

---
+++ BAEKJOON/Python/16000/16900/16933.py
@@ -0,0 +1,51 @@
+"""
+Title : 벽 부수고 이동하기 3
+Link : https://www.acmicpc.net/problem/16933
+"""
+
+from collections import deque
+from sys import stdin
+
+input = stdin.readline
+
+
+def search():
+    queue = deque([(0, 0, True, 0, 1)])
+    while queue:
+        x, y, day, wall, move = queue.popleft()
+        if wall > K:
+            continue
+        if -1 != visited[x][y][day] <= move:
+            continue
+        visited[x][y][day] = move
+        if x == N and y == M:
+            continue
+        for dx, dy in delta:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < N and 0 <= ny < M:
+                if my_map[nx][ny]:
+                    if day:
+                        queue.append((nx, ny, not day, wall + 1, move + 1))
+                    else:
+                        queue.append((x, y, not day, wall, move + 1))
+                else:
+                    queue.append((nx, ny, not day, wall, move + 1))
+
+
+if __name__ == "__main__":
+    N, M, K = map(int, input().split())
+    my_map = [[int(x) for x in input().strip()] for _ in range(N)]
+    visited = [[[-1] * 2 for _ in range(M)] for _ in range(N)]
+    wall_break = [[[N * M] * 2 for _ in range(M)] for _ in range(N)]
+    delta = ((-1, 0), (0, 1), (1, 0), (0, -1))
+    search()
+    day, night = visited[N - 1][M - 1]
+    match day, night:
+        case (-1, -1):
+            print(-1)
+        case (-1, y):
+            print(y)
+        case (x, -1):
+            print(x)
+        case (x, y):
+            print(min(x, y))
