89336320067c969db082baaec9290ef9587890ae
mintropy <jun2korea@gmail.com>
2022-09-27 16:21:11 +0900

    BOJ 12094 solving

--- BAEKJOON/Python/12000/12000/12094.py
+++ BAEKJOON/Python/12000/12000/12094.py
@@ -10,57 +10,99 @@ input = stdin.readline
 Board = list[list[int]]
 
 
-class Board:
-    def __init__(self) -> None:
-        self.max_value: int = 0
-        self.possible: list[int] = [1]
-        for _ in range(10):
-            self.possible.append(self.possible[-1] * 2)
+def dfs(max_value: int, move_count: int, board: Board, possible: list[int]) -> int:
+    if not move_count:
+        return max_value
+    for i in range(4):
+        new_board = [line[::] for line in board]
+        max_value_next, new_board = move(new_board, i)
+        if new_board == board:
+            continue
+        if max_value < (
+            possible_max_value := dfs(
+                max_value_next, move_count - 1, new_board, possible
+            )
+        ):
+            max_value = possible_max_value
+    return max_value
 
-    def rotate(self, board: Board) -> list[Board]:
-        board_list = [
-            board,
-            [line[::-1] for line in board],
-            list(list(line) for line in zip(*board)),
-            list(list(line) for line in zip(*[line for line in board[::-1]])),
-        ]
-        return board_list
 
-    def move(self, board: Board) -> Board:
-        new_board = []
-        for line in board:
-            new_line = [0] * len(line)
-            idx = 0
-            for x in line:
-                if not x:
-                    continue
-                if not new_line[idx]:
-                    new_line[idx] = x
-                elif new_line[idx] == x:
-                    new_line[idx] = x * 2
-                    idx += 1
-                else:
-                    idx += 1
-                    new_line[idx] = x
-            new_board.append(new_line)
-        return new_board
-
-    def dfs(self, move_count: int, board: Board) -> None:
-        max_value_now = max([max(line) for line in board])
-        if not move_count:
-            if self.max_value < max_value_now:
-                self.max_value = max_value_now
-            return
-        if max_value_now * self.possible[move_count] <= self.max_value:
-            return
-        for new_board in self.rotate(board):
-            if (next_board := self.move(new_board)) != board:
-                self.dfs(move_count - 1, next_board)
+def move(board: Board, d: int) -> tuple[int, Board]:
+    max_value = 0
+    if d == 0:
+        for j in range(N):
+            idx = prev = 0
+            for i in range(N):
+                if x := board[i][j]:
+                    if prev == x:
+                        max_value = max(x, prev * 2)
+                        board[idx - 1][j] = prev * 2
+                        board[i][j] = 0
+                        prev = 0
+                    else:
+                        max_value = max(x, prev)
+                        prev = board[i][j]
+                        board[i][j] = 0
+                        board[idx][j] = prev
+                        idx += 1
+    elif d == 1:
+        for i in range(N):
+            idx = N - 1
+            prev = 0
+            for j in range(N - 1, -1, -1):
+                if x := board[i][j]:
+                    if prev == x:
+                        max_value = max(x, prev * 2)
+                        board[i][idx + 1] = prev * 2
+                        board[i][j] = 0
+                        prev = 0
+                    else:
+                        max_value = max(x, prev)
+                        prev = board[i][j]
+                        board[i][j] = 0
+                        board[i][idx] = prev
+                        idx -= 1
+    elif d == 2:
+        for j in range(N):
+            idx = N - 1
+            prev = 0
+            for i in range(N - 1, -1, -1):
+                if x := board[i][j]:
+                    if prev == x:
+                        max_value = max(x, prev * 2)
+                        board[idx + 1][j] = prev * 2
+                        board[i][j] = 0
+                        prev = 0
+                    else:
+                        max_value = max(x, prev)
+                        prev = board[i][j]
+                        board[i][j] = 0
+                        board[idx][j] = prev
+                        idx -= 1
+    elif d == 3:
+        for j in range(N):
+            idx = prev = 0
+            for i in range(N):
+                if x := board[i][j]:
+                    if prev == x:
+                        max_value = max(x, prev * 2)
+                        board[i][idx - 1] = prev * 2
+                        board[i][j] = 0
+                        prev = 0
+                    else:
+                        max_value = max(x, prev)
+                        prev = board[i][j]
+                        board[i][j] = 0
+                        board[i][idx] = prev
+                        idx += 1
+    return max_value, board
 
 
 if __name__ == "__main__":
-    N = int(input())
-    board = [list(map(int, input().split())) for _ in range(N)]
-    board2048 = Board()
-    board2048.dfs(10, board)
-    print(board2048.max_value)
+    N: int = int(input())
+    board: Board = [list(map(int, input().split())) for _ in range(N)]
+    max_value: int = max([max(line) for line in board])
+    possible: list[int] = [1]
+    for _ in range(10):
+        possible.append(possible[-1] * 2)
+    print(dfs(max_value, 10, board, possible))
