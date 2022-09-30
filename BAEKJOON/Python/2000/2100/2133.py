cf7c4f2a40fea346e38c305694c42cf618af6247
mintropy <jun2korea@gmail.com>
2022-09-30 09:13:11 +0900

    BOJ 2133 solved Python

--- BAEKJOON/Python/2000/2100/2133.py
+++ BAEKJOON/Python/2000/2100/2133.py
@@ -8,17 +8,19 @@ from sys import stdin
 input = stdin.readline
 
 
+def solve(N: int) -> int:
+    if N % 2:
+        return 0
+    dp = [0] * (N // 2 + 1)
+    dp[0] = 1
+    for i in range(1, N // 2 + 1):
+        tmp = dp[i - 1] * 3
+        for j in range(0, i - 1):
+            tmp += dp[j] * 2
+        dp[i] = tmp
+    return dp[-1]
+
+
 if __name__ == "__main__":
     N = int(input())
-    if N % 2:
-        print(0)
-    else:
-        dp = [0] * (N // 2 + 1)
-        dp[0] = 1
-        dp[1] = 3
-        for i in range(2, N // 2 + 1):
-            tmp = 0
-            for j in range(i):
-                tmp += dp[j] * dp[i - 1 - j] * 3
-            dp[i] = tmp
-        print(dp[-1])
+    print(solve(N))
