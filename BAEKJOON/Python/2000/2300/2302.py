59cef4fcf6705c2d286e9c00b469ee40b70be7d4
mintropy <jun2korea@gmail.com>
2022-10-03 09:59:35 +0900

    BOJ 2302 solved Python

--- BAEKJOON/Python/2000/2300/2302.py
+++ BAEKJOON/Python/2000/2300/2302.py
@@ -9,24 +9,22 @@ input = stdin.readline
 II = lambda: int(input())
 
 
+def solve(N: int, M: int, seats: list[int]) -> int:
+    dp = [0] * (41)
+    dp[0], dp[1], dp[2] = 1, 1, 2
+    for i in range(3, N + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+    seats += [0, N + 1]
+    seats.sort()
+    ans = 1
+    for i in range(M + 1):
+        x, y = seats[i], seats[i + 1]
+        ans *= dp[y - x - 1]
+    return ans
+
+
 if __name__ == "__main__":
     N: int = II()
     M: int = II()
     vips: list[int] = [II() for _ in range(M)] + [N + 1]
-
-    dp: list[list[int]] = [[0] * 2 for _ in range(N + 1)]
-    dp[0] = [1, 0]
-    vip_idx: int = 0
-    last_vip: bool = True
-    for i in range(1, N + 1):
-        if i == vips[vip_idx]:
-            dp[i] = dp[i - 1]
-            vip_idx += 1
-            last_vip = True
-            continue
-        if last_vip:
-            dp[i] = dp[i - 1]
-            last_vip = False
-        else:
-            dp[i] = [sum(dp[i - 1]), dp[i - 1][0]]
-    print(sum(dp[-1]))
+    print(solve(N, M, vips))
