9cac2cfc99fee8942dc6906de661e8add4f94ddc
mintropy <jun2korea@gmail.com>
2022-10-05 14:56:49 +0900

    BOJ 1725 solved Python

--- BAEKJOON/Python/1000/1700/1725.py
+++ BAEKJOON/Python/1000/1700/1725.py
@@ -6,6 +6,16 @@ Link : https://www.acmicpc.net/problem/1725
 from sys import stdin
 
 input = stdin.readline
+II = lambda: int(input())
 
 if __name__ == "__main__":
-    pass
+    N = II()
+    histogram = [0] + [II() for _ in range(N)] + [0]
+    stack = [0]
+    ans = 0
+    for i in range(1, N + 2):
+        while stack and histogram[stack[-1]] > histogram[i]:
+            h = stack.pop()
+            ans = max(ans, histogram[h] * (i - stack[-1] - 1))
+        stack.append(i)
+    print(ans)
