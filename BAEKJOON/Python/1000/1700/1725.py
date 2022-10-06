c8782fc259d15ecc271d661402a583a3a78225a1
mintropy <jun2korea@gmail.com>
2022-10-06 12:13:59 +0900

    BOJ 1725 solved Python - div & conquer

--- BAEKJOON/Python/1000/1700/1725.py
+++ BAEKJOON/Python/1000/1700/1725.py
@@ -19,3 +19,52 @@ if __name__ == "__main__":
             ans = max(ans, histogram[h] * (i - stack[-1] - 1))
         stack.append(i)
     print(ans)
+
+
+# ------------------------------------------------------------
+
+from sys import stdin
+
+input = stdin.readline
+II = lambda: int(input())
+
+
+def div_conquer(N: int, histogram: list[int], left: int, right: int) -> int:
+    if left == right:
+        return histogram[left]
+    mid = (left + right) // 2
+    l = r = mid
+    h = histogram[mid]
+    w = 1
+    ans = h
+    while l > left and r < right:
+        if (h_l := histogram[l - 1]) >= (h_r := histogram[r + 1]):
+            l -= 1
+            w += 1
+            h = min(h, h_l)
+        else:
+            r += 1
+            w += 1
+            h = min(h, h_r)
+        ans = max(ans, h * w)
+    while l > left:
+        l -= 1
+        w += 1
+        h = min(h, histogram[l])
+        ans = max(ans, w * h)
+    while r < right:
+        r += 1
+        w += 1
+        h = min(h, histogram[r])
+        ans = max(ans, w * h)
+    return max(
+        ans,
+        div_conquer(N, histogram, left, mid),
+        div_conquer(N, histogram, mid + 1, right),
+    )
+
+
+if __name__ == "__main__":
+    N = II()
+    histogram = [II() for _ in range(N)]
+    print(div_conquer(N, histogram, 0, N - 1))
