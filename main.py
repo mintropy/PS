2055d78a43fdf8190d4b99484c8a78f25e8766c4
mintropy <jun2korea@gmail.com>
2022-10-07 21:11:28 +0900

    file creater update, only BOJ, Python

--- main.py
+++ main.py
@@ -4,8 +4,11 @@
 
 import os
 
+import requests
+from bs4 import BeautifulSoup
 
-boj_python_template = '"""\nTitle : \nLink : \n"""'
+boj_url = "https://www.acmicpc.net/problem/"
+# boj_python_template = '"""\nTitle : \nLink : \n"""'
 
 
 def main() -> None:
@@ -17,6 +20,13 @@ def main() -> None:
     if platform == "BOJ":
         problem_num = int(input("problem number\n").strip())
         # 문제 제목 가져오기
+        url = boj_url + f"{problem_num}"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        res = soup.select_one("#problem_title").get_text()
+        boj_python_template = f'"""\nTitle : {res}\nLink : {url}\n"""\n'
+        python_template = "from sys import stdin\n\ninput = stdin.readline\n\n\n"
+
         problem_dir = f"{problem_num // 1000 * 1000}/{problem_num // 100 * 100}"
         languages = input("languages (py go js)\n").strip().split()
         languages_name = {
@@ -30,6 +40,8 @@ def main() -> None:
                 os.makedirs(target_dir)
             # 파일 생성
             # 파일에 탬플릿 적용
+            with open(os.path.join(target_dir, f"{problem_num}.{language}"), "w") as fp:
+                fp.write(boj_python_template + "\n" + python_template)
 
 
 if __name__ == "__main__":
