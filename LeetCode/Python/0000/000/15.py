3630f9e61d7c743233f7dd7858d0adba6e7f6e6a
mintropy <jun2korea@gmail.com>
2022-10-19 15:19:30 +0900

    LeetCode 15 solved Python

--- LeetCode/Python/0000/000/15.py
+++ LeetCode/Python/0000/000/15.py
@@ -9,33 +9,29 @@ from typing import List
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        left, right = 0, len(nums) - 1
         ans = set()
-        while right - left >= 2:
-            x, y = nums[left], nums[right]
-            tmp = x + y
-            for i in range(left + 1, right):
-                z = nums[i]
-                if tmp + z == 0:
-                    ans.add((x, z, y))
-                elif tmp + z > 0:
-                    break
-            if tmp == 0:
-                if nums[left + 1] - x > y - nums[right - 1]:
+        for i, x in enumerate(nums):
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                tmp = x + nums[left] + nums[right]
+                if tmp < 0:
+                    left += 1
+                elif tmp > 0:
                     right -= 1
                 else:
+                    ans.add((x, nums[left], nums[right]))
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
                     left += 1
-            elif tmp > 0:
-                right -= 1
-            else:
-                left += 1
+                    right -= 1
         return list(ans)
 
 
 if __name__ == "__main__":
     nums = [-1, 0, 1, 2, -1, -4]
     nums = [-2, 0, 1, 1, 2]
-    nums = [-1, 0, 1, 2, -1, -4]
 
     solution = Solution()
     print(solution.threeSum(nums))
