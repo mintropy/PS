a48d330b8b975eec77faa286373f5640251182a0
mintropy <jun2korea@gmail.com>
2022-10-20 09:53:24 +0900

    LeetCode 2 solved Python

--- LeetCode/Python/0000/000/2.py
+++ LeetCode/Python/0000/000/2.py
@@ -16,14 +16,21 @@ class Solution:
     def addTwoNumbers(
         self, l1: Optional[ListNode], l2: Optional[ListNode]
     ) -> Optional[ListNode]:
-        num1 = num2 = 0
+        num1, num2 = [], []
         while l1 is not None:
-            num1 = num1 * 10 + l1.val
+            num1.append(l1.val)
             l1 = l1.next
         while l2 is not None:
-            num2 = num2 * 10 + l2.val
+            num2.append(l2.val)
             l2 = l2.next
-        num = num1 + num2
+        num1.reverse()
+        num2.reverse()
+        n1 = n2 = 0
+        for x in num1:
+            n1 = n1 * 10 + x
+        for x in num2:
+            n2 = n2 * 10 + x
+        num = n1 + n2
         new_list = []
         if not num:
             new_list.append(0)
@@ -39,4 +46,20 @@ class Solution:
 
 
 if __name__ == "__main__":
-    pass
+
+    def make_linked_list(nums: list[int]) -> ListNode:
+        node = ListNode(nums[0])
+        now = node
+        for x in nums[1:]:
+            now.next = ListNode(x)
+            now = now.next
+        return node
+
+    list1 = make_linked_list([2, 4, 9])
+    list2 = make_linked_list([5, 6, 4, 9])
+
+    list1 = make_linked_list([0, 8, 6, 5, 6, 8, 3, 5, 7])
+    list2 = make_linked_list([6, 7, 8, 0, 8, 5, 8, 9, 7])
+
+    solution = Solution()
+    print(solution.addTwoNumbers(list1, list2))
