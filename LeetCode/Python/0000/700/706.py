8064ddde2c545b9002447c56cb707b4e46d41efd
mintropy <jun2korea@gmail.com>
2022-10-27 15:16:55 +0900

    LeetCode 706 solved Python

---
+++ LeetCode/Python/0000/700/706.py
@@ -0,0 +1,25 @@
+"""
+Title : design hashmap
+Link : https://leetcode.com/problems/design-hashmap/submissions/
+"""
+
+class MyHashMap:
+    def __init__(self):
+        self.hash_map = dict()
+
+    def put(self, key: int, value: int) -> None:
+        self.hash_map[key] = value
+
+    def get(self, key: int) -> int:
+        return self.hash_map.get(key, -1)
+
+    def remove(self, key: int) -> None:
+        if key in self.hash_map:
+            self.hash_map.pop(key)
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
