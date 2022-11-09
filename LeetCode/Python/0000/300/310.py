9813a565a803af9e99b6bdd3aca67becb16eedd8
mintropy <jun2korea@gmail.com>
2022-11-09 14:58:41 +0900

    LeetCode 310 solving Python

---
+++ LeetCode/Python/0000/300/310.py
@@ -0,0 +1,15 @@
+"""
+Title : Minimum Height Trees
+Link : https://leetcode.com/problems/minimum-height-trees/
+"""
+
+from typing import List
+
+
+class Solution:
+    def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+        pass
+
+
+if __name__ == "__main__":
+    pass
