1ce48e1c3534573a47788eb903540f63f93f7358
mintropy <jun2korea@gmail.com>
2022-11-09 14:56:22 +0900

    LeetCode 110 solving Python

---
+++ LeetCode/Python/0000/100/110.py
@@ -0,0 +1,22 @@
+"""
+Title : Balanced Binary Tree
+Link : https://leetcode.com/problems/balanced-binary-tree/
+"""
+
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        pass
+
+
+if __name__ == "__main__":
+    pass
