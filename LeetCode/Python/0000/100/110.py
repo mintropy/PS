ca681fcfed5b0696395265e3c717656b3bddb894
mintropy <jun2korea@gmail.com>
2022-11-10 16:27:32 +0900

    LeetCode 110 solved Python

--- LeetCode/Python/0000/100/110.py
+++ LeetCode/Python/0000/100/110.py
@@ -15,7 +15,18 @@ class TreeNode:
 
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        pass
+        return self.search(root, 1)[0]
+
+    def search(self, node: Optional[TreeNode], depth):
+        if node is None:
+            return True, depth - 1
+        left_condition, left_depth = self.search(node.left, depth + 1)
+        right_condition, right_depth = self.search(node.right, depth + 1)
+        if not left_condition or not right_condition:
+            return False, 0
+        if abs(left_depth - right_depth) <= 1:
+            return True, max(left_depth, right_depth)
+        return False, 0
 
 
 if __name__ == "__main__":
