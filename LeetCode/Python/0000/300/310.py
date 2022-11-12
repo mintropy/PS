3eaaa91f0212a776e13e511f0a9dd8be5b3077a0
mintropy <jun2korea@gmail.com>
2022-11-12 22:03:41 +0900

    LeetCode 310 soving TLE

--- LeetCode/Python/0000/300/310.py
+++ LeetCode/Python/0000/300/310.py
@@ -4,11 +4,36 @@ Link : https://leetcode.com/problems/minimum-height-trees/
 """
 
 from typing import List
+from collections import deque
 
 
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
-        pass
+        if n <= 2:
+            return 1
+        graph = [[] for _ in range(n)]
+        for x, y in edges:
+            graph[x].append(y)
+            graph[y].append(x)
+        node = self.find_node(n, 0, graph)[0]
+        nodes = self.find_node(n, node, graph)
+        nodes.append(node)
+        return nodes
+
+    def find_node(self, n, st, graph):
+        queue = [st]
+        ans = []
+        visited = [False] * n
+        while queue:
+            ans = queue[::]
+            next_queue = set()
+            for x in queue:
+                for y in graph[x]:
+                    if not visited[y]:
+                        next_queue.add(y)
+            next_queue = list(next_queue)
+            queue = next_queue[::]
+        return ans
 
 
 if __name__ == "__main__":
