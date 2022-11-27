5bfd3698965f697887540e1e008474abee2888da
mintropy <jun2korea@gmail.com>
2022-11-27 19:15:03 +0900

    BOJ 1254 solved Python

--- BAEKJOON/Python/1000/1200/1254.py
+++ BAEKJOON/Python/1000/1200/1254.py
@@ -11,14 +11,39 @@ input = stdin.readline
 if __name__ == "__main__":
     s = input().strip()
     l = len(s)
-    if l == 1 or (l == 2 and s[0] == s[1]):
-        print(0)
+    if s == s[::-1]:
+        print(l)
         exit()
-    for mid in range(l // 2, l):
+    if l == 1:
+        print(1)
+        exit()
+    if l == 2:
+        if s[0] == s[1]:
+            print(2)
+        else:
+            print(3)
+        exit()
+
+    for mid in range(l // 2 + l % 2, l):
         if s[mid] == s[mid - 1]:
             for i in range(l - mid - 1):
                 if s[mid + i + 1] != s[mid - i - 2]:
                     break
             else:
-                print(l - mid * 2)
-        
+                print(l + mid * 2 - l)
+                break
+        for i in range(l - mid - 1):
+            if s[mid + i + 1] != s[mid - i - 1]:
+                break
+        else:
+            print(l + mid * 2 - l + 1)
+            break
+
+
+"""
+Counter Example
+
+eqqee
+out : 4
+ans : 8
+"""
