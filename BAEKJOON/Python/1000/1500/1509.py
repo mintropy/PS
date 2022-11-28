ca1a96cb595630842148586fc78b42a44fad0793
mintropy <jun2korea@gmail.com>
2022-11-28 22:28:22 +0900

    BOJ 1509 solved Python

--- BAEKJOON/Python/1000/1500/1509.py
+++ BAEKJOON/Python/1000/1500/1509.py
@@ -4,10 +4,18 @@ Link : https://www.acmicpc.net/problem/1509
 """
 
 from sys import stdin
+
 input = stdin.readline
 
 
 if __name__ == "__main__":
-    s = input().strip()
+    s = " " + input().strip()
     l = len(s)
-    dp = [0] * l
+    dp = [i for i in range(l)]
+    for i in range(2, l):
+        dp[i] = dp[i - 1] + 1
+        for j in range(i - 1, 0, -1):
+            tmp = s[j : i + 1]
+            if tmp == tmp[::-1]:
+                dp[i] = min(dp[i], dp[j - 1] + 1)
+    print(dp[-1])
