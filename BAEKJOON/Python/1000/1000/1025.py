b11ec6225211d3be7d8a109c3ac6abb1c87b78a4
mintropy <jun2korea@gmail.com>
2023-04-11 19:34:24 +0900

    BOJ 1025 solving

--- BAEKJOON/Python/1000/1000/1025.py
+++ BAEKJOON/Python/1000/1000/1025.py
@@ -3,10 +3,29 @@ Title : 제곱수 찾기
 Link : https://www.acmicpc.net/problem/1025
 """
 
-import sys
-input = sys.stdin.readline
+from sys import stdin
+
+input = stdin.readline
+
+
+def square_validation(positions: list[tuple[int]]) -> bool:
+    pass
 
 
 if __name__ == "__main__":
     N, M = map(int, input().split())
-    numbers = []
+    number_map: list[list[int]] = [
+        list(int(x) for x in input().strip()) for _ in range(N)
+    ]
+    numbers: dict[int, list[tuple[int]]] = {x: [] for x in range(1, 10)}
+    for i, line in enumerate(number_map):
+        for j, x in enumerate(line):
+            numbers[x].append(i, j)
+
+    max_square: int = 9 if numbers[9] else 4 if numbers[4] else 1 if numbers[1] else -1
+    for x in range(4, int(max(N, M) ** 0.5) + 1):
+        square = x * x
+        positions = []
+        while square:
+            positions.append(square % 10)
+            square //= 10
