f7e0db9c3db7ef85f95ba2da453438b27d07a49e
mintropy <jun2korea@gmail.com>
2023-04-27 20:46:57 +0900

    BOJ 1174 solving

---
+++ BAEKJOON/Python/1000/1100/1174.py
@@ -0,0 +1,21 @@
+"""
+Title : 줄어드는 수
+Link : https://www.acmicpc.net/problem/1174
+"""
+
+from sys import stdin
+
+input = stdin.readline
+
+
+def search(N: int, num: list[int]) -> int:
+    if N == 1:
+        return num
+
+
+def next_num(num: list[int]) -> list[int]:
+    pass
+
+
+if __name__ == "__main__":
+    N = int(input())
