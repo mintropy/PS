e514cb7a040e7a67b756275b23fce3e74722cd85
mintropy <jun2korea@gmail.com>
2023-05-03 21:28:08 +0900

    BOJ 1394 solving

--- BAEKJOON/Python/1000/1300/1394.py
+++ BAEKJOON/Python/1000/1300/1394.py
@@ -12,9 +12,14 @@ IS = lambda: input().strip()
 if __name__ == "__main__":
     total: str = IS()
     secrect: str = IS()
+    base: int = len(total)
 
-    positions = {x: idx + 1 for idx, x in enumerate(total)}
+    positions = {x: idx for idx, x in enumerate(total)}
     ans: int = 0
+
+    for idx, x in enumerate(secrect[::-1]):
+        pass
+
     for x in secrect:
         ans *= len(total)
         ans += positions[x]
